4debf733409c250e3bf435525edc0aed8f7159b0
Manuel <mrctinio@gmail.com>
2021-03-27 09:56:09 +0800

    edited rspec test titles, removed active_storage :test

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,28 +4,28 @@ RSpec.describe Review, type: :model do
   let(:review) { described_class.new }
 
   describe 'Review validations' do
-    it 'Does not save if title is not present' do
+    it 'does not save if title is not present' do
       review.rating = 5
       review.body = 'Great test!'
 
       expect(review).not_to be_valid
     end
 
-    it 'Does not save if rating is not present' do
+    it 'does not save if rating is not present' do
       review.title = 'Testing'
       review.body = 'Great test!'
 
       expect(review).not_to be_valid
     end
 
-    it 'Does not save if body is not present' do
+    it 'does not save if body is not present' do
       review.title = 'Testing'
       review.rating = 5
 
       expect(review).not_to be_valid
     end
 
-    it 'Does not save if rating is < 5' do
+    it 'does not save if rating is < 5' do
       review.title = 'Testing'
       review.rating = 6
       review.body = 'Great test!'
@@ -33,7 +33,7 @@ RSpec.describe Review, type: :model do
       expect(review).not_to be_valid
     end
 
-    it 'Does not save if rating is >= 0' do
+    it 'does not save if rating is >= 0' do
       review.title = 'Testing'
       review.rating = 0
       review.body = 'Great test!'
