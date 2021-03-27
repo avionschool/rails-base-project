11ed5be09a9419281bacf5bd61c31bb3f460de41
Wesley Dela Cruz <wesleydelacruz97@gmail.com>
2021-03-27 09:13:03 +0800

    changed 'should' to 'is_expected.to' in rspec

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Service, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_length_of(:name).is_at_most(50) }
-    it { should validate_presence_of(:price) }
-    it { should validate_numericality_of(:price).is_greater_than(0) }
-    it { should validate_presence_of(:type) }
-    it { should validate_length_of(:type).is_at_most(50) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_most(50) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
+    it { is_expected.to validate_presence_of(:type) }
+    it { is_expected.to validate_length_of(:type).is_at_most(50) }
   end
 end
