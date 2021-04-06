6bc4cae4ebfb3899e2896b6e8614e6493f170f01
Wesley Dela Cruz <wesleydelacruz97@gmail.com>
2021-04-06 21:06:25 +0800

    set up supplier show action and view

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root to: "welcome#index"
 
   resources :buyers
+  resources :admins
 
   resources :suppliers, shallow: true do
     resources :services do
@@ -13,9 +14,7 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :admins
-  resources :suppliers
-  resources :buyers
+
   
   get 'admins/pending/supplier' => 'admins#pending_supplier_signup', as: 'pending_supplier_signup_admin'
   put 'admins/:id/approve_supplier' => 'admins#approve_supplier_signup', as: 'approve_supplier_admin'
