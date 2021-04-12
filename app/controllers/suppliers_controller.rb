b6e5f6084921920bfe07814845fd7b5372c357a4
Wesley Dela Cruz <wesleydelacruz97@gmail.com>
2021-04-12 12:28:16 +0800

    Update suppliers_controller.rb

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,6 +1,6 @@
 class SuppliersController < ApplicationController
   def show
     @supplier = Supplier.find(params[:id])
-    @services = @supplier.services.all
+    @services = @supplier.services
   end
 end
