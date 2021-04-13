b4a84799da9ff6b238a2fe7dd4401b6e0ef1b1b0
Wesley Dela Cruz <wesleydelacruz97@gmail.com>
2021-04-13 22:37:27 +0800

    set up method for downcasing category field upon saving  - WIP

--- app/models/service.rb
+++ app/models/service.rb
@@ -20,9 +20,14 @@ class Service < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :category, presence: true, length: { maximum: 50 }, inclusion: { in: VALID_CATEGORIES }
+  # before_save :downcase_fields
 
   belongs_to :supplier
   has_many :buyer_services, dependent: :destroy
   has_many :buyers, through: :buyer_services
   has_many :reviews, dependent: :destroy
+
+  def downcase_fields
+    self.category.downcase
+  end
 end
