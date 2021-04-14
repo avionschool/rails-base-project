8782aabecb2f15ba30c1cbdb92a657d5b8480e08
Dave <ddcmendoza@gmail.com>
2021-04-14 21:02:15 +0800

    Model Updates (see previous commit)

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,3 @@
 class Stock < ApplicationRecord
-    
+    belongs_to :user, class_name: "user", foreign_key: "user_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
     has_and_belongs_to_many :role, foreign_key: "role_id"
-    has_many :stock, class_name: "stock", foreign_key: "stock_id"
+    has_and_belongs_to_many :stock, join_table: "buyers_stocks", foreign_key: "stock_id"
 end
