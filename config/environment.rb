4794432e8bd9f7b6560d91c35ea7344203eb2b0d
Dave <ddcmendoza@gmail.com>
2021-04-14 21:02:46 +0800

    [WIP] IEX Interfacing

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 
+token = File.join(Rails.root, 'config', 'token.rb')
+load(token) if File.exist?(token)
 # Initialize the Rails application.
 Rails.application.initialize!
