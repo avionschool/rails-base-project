7710805ce9ac7f909a5816e4318760994036c0ff
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-16 22:00:02 +0800

    created app models (initial commit)

---
+++ app/models/broker.rb
@@ -0,0 +1,7 @@
+class Broker < ApplicationRecord
+  belongs_to :admin
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
