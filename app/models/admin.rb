d9e6fe521f6b3565b4258ec1aa71ff343ae15b0c
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-16 22:18:53 +0800

    added validations for email presence

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,7 @@
 class Admin < ApplicationRecord
   has_many :brokers
   has_many :buyers
+  validates :email, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- app/models/broker.rb
+++ app/models/broker.rb
@@ -1,5 +1,6 @@
 class Broker < ApplicationRecord
   belongs_to :admin
+  validates :email, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
