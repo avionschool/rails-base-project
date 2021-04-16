f2527b5b21717d8b1e65938125440047015b9cfd
Dave <ddcmendoza@gmail.com>
2021-04-16 19:03:28 +0800

    [WIP] Added Devise Authentication to User
    -Removed unnecessary migration files for Rails update (to 6.1.0)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     has_and_belongs_to_many :role, foreign_key: "role_id"
     has_and_belongs_to_many :stock, join_table: "buyers_stocks", foreign_key: "stock_id"
 end
