fbf34f9ae8413996b2e76b1a53669f8ca7f25479
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-17 12:41:32 +0800

    added account routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :brokers, :controllers => { :registrations => :registrations }
-  devise_for :buyers, :controllers => { :registrations => :registrations }
+  devise_for :admins, :controllers => { :sessions => :sessions }
+  devise_for :brokers, :controllers => { :registrations => :registrations, :sessions => :sessions }
+  devise_for :buyers, :controllers => { :registrations => :registrations, :sessions => :sessions }
   root 'home#index'
+  get '/admin' => 'admin#index'
+  get '/buyer' => 'buyer#index'
+  get '/broker' => 'broker#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
