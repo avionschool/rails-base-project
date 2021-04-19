8ce52cfe194a7668912ecdf8d8b93486f54445dc
Dave <ddcmendoza@gmail.com>
2021-04-19 19:17:16 +0800

    [WIP] Model Testing
    Model Updates:
    BuyersStock:
    --Added class method total_stock
    ---computes total available stock in circulation
    
    Stock:
    -- Added instance method check_total
    --- updates stock attribute of Stock
    
    Transaction:
    -- Added a check_stock_total check before save
    --- runs the check_total method for every stock involved
    
    User:
    -- Formatting change
    
    Testing:
    
    Added tests for methods

--- app/models/buyers_stock.rb
+++ app/models/buyers_stock.rb
@@ -1,4 +1,5 @@
 class BuyersStock < ApplicationRecord
+
     def self.find_entry(user_id,stock_id)
         self.where(stock_id: stock_id).find_by(user_id: user_id)
     end
@@ -11,4 +12,7 @@ class BuyersStock < ApplicationRecord
         self.find_entry(user_id,stock_id).volume
     end
 
+    def self.total_stock(stock)
+        self.where(stock_id: stock.id).sum(:volume) + self.where(stock_id: stock.id).sum(:alloted_volume)
+    end
 end
