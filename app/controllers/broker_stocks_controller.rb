f24e7314a5b5aacb75d87e062e164be77a79f518
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-19 15:47:29 +0800

    created BrokerStocks  controller w/ create action

---
+++ app/controllers/broker_stocks_controller.rb
@@ -0,0 +1,14 @@
+class BrokerStocksController < ApplicationController
+  
+  def create
+    stock = Stock.new_search(params[:ticker])
+    if stock.blank?
+      stock = Stock.new_search(params[:ticker])
+      stock.save
+    end
+    @broker_stock = BrokerStock.create(broker: current_broker, stock: stock)
+    flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
+    redirect_to broker_portfolio_path
+  end
+  
+end
