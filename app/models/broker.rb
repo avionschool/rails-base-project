266b00bcba0daf09d3852e47d394d507f3c85a01
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-19 15:50:31 +0800

    created methods for checking if stock can be added

--- app/models/broker.rb
+++ app/models/broker.rb
@@ -1,8 +1,22 @@
 class Broker < ApplicationRecord
+  has_many :broker_stocks
+  has_many :stocks, through: :broker_stocks
   belongs_to :admin
   validates :email, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  def stock_already_added?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    return false unless stock
+    stocks.where(id: stock.id).exists?
+  end
+
+  def can_add_stock?(ticker_symbol)
+    !stock_already_added?(ticker_symbol)
+  end
 end
