0af34f2aa7f6f6fd33704272d63cc216a2e1ee5a
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-19 15:53:46 +0800

    added routes for broker pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  resources :broker_stocks, only: [:create]
   devise_for :admins, :controllers => { :sessions => :sessions }
   devise_for :brokers, :controllers => { :registrations => :registrations, :sessions => :sessions }
   devise_for :buyers, :controllers => { :registrations => :registrations, :sessions => :sessions }
   root 'home#index'
-  get '/admin' => 'admin#index'
-  get '/buyer' => 'buyer#index'
-  get '/broker' => 'broker#index'
+  get 'search_stock' => 'stocks#stock_search'
+  get '/admin/index' => 'admin#index'
+  get '/buyer/index' => 'buyer#index'
+
+  # Broker
+  get '/broker/index' => 'broker#index'
+  get '/broker/portfolio' => 'broker#portfolio'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
