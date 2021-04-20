bc491fc3346ab9f15e829f182eec6ca142eec539
Elijah Arcedera <eli.arcedera@gmail.com>
2021-04-20 20:53:59 +0800

    fixed indentations

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,23 +6,23 @@ class Stock < ApplicationRecord
     validates :name, :ticker, presence: true
 
     def self.new_search(ticker_symbol)
-        client = IEX::Api::Client.new(
-            publishable_token: ENV["IEX_PUBLISHABLE_KEY"],
-            secret_token: ENV["IEX_SECRET_KEY"],
+      client = IEX::Api::Client.new(
+        publishable_token: ENV["IEX_PUBLISHABLE_KEY"],
+        secret_token: ENV["IEX_SECRET_KEY"],
             # publishable_token: Rails.application.credentials.iex_client[:publishable_access_key],
             # secret_token: Rails.application.credentials.iex_client[:secret_access_key],
-            endpoint: 'https://sandbox.iexapis.com/v1'
-          )
-        begin
-            #ticker is upcased because if not, it doesn't recognize that I've already added the same stock
-            new(ticker: ticker_symbol.upcase, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
-        rescue => exception
-            return nil
-        end
+        endpoint: 'https://sandbox.iexapis.com/v1'
+        )
+      begin
+          #ticker is upcased because if not, it doesn't recognize that I've already added the same stock
+        new(ticker: ticker_symbol.upcase, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+      rescue => exception
+        return nil
+      end
     end
 
     def self.check_db(ticker_symbol, broker_id)
-        find_by(ticker: ticker_symbol, added_by: broker_id)
+      find_by(ticker: ticker_symbol, added_by: broker_id)
     end
     
     # Doesn't work in Sandbox ENV
@@ -37,4 +37,15 @@ class Stock < ApplicationRecord
     #     logo = client.logo(ticker_symbol)
     #     logo.url
     # end
+
+    def self.stock_list
+      client = IEX::Api::Client.new(
+        publishable_token: ENV["IEX_PUBLISHABLE_KEY"],
+        secret_token: ENV["IEX_SECRET_KEY"],
+                # publishable_token: Rails.application.credentials.iex_client[:publishable_access_key],
+                # secret_token: Rails.application.credentials.iex_client[:secret_access_key],
+        endpoint: 'https://sandbox.iexapis.com/v1'
+        )
+      client.stock_market_list(:mostactive) # [{symbol: 'AAPL', ...}, {...}]
+    end
 end
