97233d51e2155505804630fb02e9736ccde478f9
Dave <ddcmendoza@gmail.com>
2021-04-24 10:56:57 +0800

    [WIP] Added Stocks Controller.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,6 +4,19 @@ class TransactionsController < ApplicationController
   end
 
   def show; end
+  
+  def new 
+    case params[:transaction_type]
+    when 'Buy'
+        current_user.buy_stock(Stock.find(params[:stock_id]).code, params[:volume], params[:price])
+    when 'Sell'
+        current_user.sell_stock(Stock.find(params[:stock_id]).code, params[:volume], params[:price])
+    end
+  end
+
+  def update
+    current_user.process_transaction(@transaction, params[:volume])
+  end
 
   private
 
