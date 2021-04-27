ef7593d096b8016193543fb248e624a4ac45f498
SamDg <sam.dg019@gmail.com>
2021-04-28 01:59:24 +0800

    Adds FactoryBot files and controllers rspec tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,18 +1,17 @@
-FactoryBot.define do 
-    factory :user do
+FactoryBot.define do
+  factory :user do
+    trait(:buyer) { role_id { 1 } }
+    trait(:broker) { role_id { 2 } }
 
-        trait(:buyer) { role_id {1} }
-        trait(:broker) { role_id {2} }
+    firstname { 'person' }
+    lastname { 'example' }
 
-        firstname { "person" }
-        lastname { "example" }
-
-        sequence :email do |n|
-        "person#{n}@example.com"
-        end
-
-        password {'123456'}
-        
-        confirmed_at { DateTime.now }
+    sequence :email do |n|
+      "person#{n}@example.com"
     end
+
+    password { '123456' }
+
+    confirmed_at { DateTime.now }
+  end
 end
--- spec/features/user_login.rb
+++ spec/features/user_login.rb
@@ -1,36 +1,30 @@
 require 'rails_helper'
 
-
 RSpec.describe 'Login as a User', type: :feature do
-    let(:buyer) {create :user, :buyer}
-    let(:broker) {create :user, :broker}
+  let(:buyer) { create :user, :buyer }
+  let(:broker) { create :user, :broker }
 
-    it 'signs the buyer user in' do
-      visit new_user_session_path
+  it 'buyer sign in' do
+    visit new_user_session_path
 
-      within "#new_user" do 
+    within '#new_user' do
       fill_in 'user_email', with: buyer.email
       fill_in 'user_password', with: buyer.password
 
       click_on('Log in')
       expect(page).to have_current_path(buyers_path)
-
-      end
-    
     end
+  end
 
+  it 'broker sign in' do
+    visit new_user_session_path
 
-    it 'signs the broker user in' do
-      visit new_user_session_path
-
-      within "#new_user" do 
+    within '#new_user' do
       fill_in 'user_email', with: broker.email
       fill_in 'user_password', with: broker.password
 
       click_on('Log in')
       expect(page).to have_current_path(brokers_path)
-
-      end
-    
     end
+  end
 end
