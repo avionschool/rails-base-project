822e07bcbdf83453da33489ea3f11664d337d6b6
Dave <ddcmendoza@gmail.com>
2021-05-02 22:22:36 +0800

    Fixed early rendering issue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,16 +5,16 @@ class HomeController < ApplicationController
   end
 
   def show
-    current_user.admin? ? (redirect_to admin_path) : (render :show)
     @portfolio = BuyersStock.where(user_id: current_user.id).collect do |x|
       {
         stock_id: x.stock_id,
-          name: Stock.find(x.stock_id).name,
-          volume: x.volume,
-          price: Stock.find(x.stock_id).price,
-          code: Stock.find(x.stock_id).code
+        name: Stock.find(x.stock_id).name,
+        volume: x.volume,
+        price: Stock.find(x.stock_id).price,
+        code: Stock.find(x.stock_id).code
       }
     end
     @top10 = Stock.most_active
+    current_user.admin? ? (redirect_to admin_path) : (render :show)
   end
 end
