4b705a162ccccfe1e8dc2071f86a43f97b9d48ba
AlexCitrus <kidong044@gmail.com>
2021-05-02 17:03:10 +0800

    [HOTFIX] Adds instance of IEX to stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,6 @@
 class Stock < ApplicationRecord
-  require "iex-ruby-client"
-  has_and_belongs_to_many :users, join_table: "buyers_stocks"
+  require 'iex-ruby-client'
+  has_and_belongs_to_many :users, join_table: 'buyers_stocks'
   has_many :transactions, dependent: :nullify
 
   validates :volume, numericality: { greater_than_or_equal_to: 0 }
@@ -13,11 +13,11 @@ class Stock < ApplicationRecord
 
   def self.most_active
     @client = IEX::Api::Client.new(
-      publishable_token: ENV["IEX_API_PUBLISHABLE_TOKEN"],
-      secret_token: ENV["IEX_API_SECRET_TOKEN"],
-      endpoint: "https://cloud.iexapis.com/v1",
+      publishable_token: ENV['IEX_API_PUBLISHABLE_TOKEN'],
+      secret_token: ENV['IEX_API_SECRET_TOKEN'],
+      endpoint: 'https://cloud.iexapis.com/v1'
     )
-    Rails.cache.fetch("active_stocks", expires_in: 12.hours) do
+    Rails.cache.fetch('active_stocks', expires_in: 12.hours) do
       @client.stock_market_list(:mostactive)
     end
   end
@@ -29,9 +29,9 @@ class Stock < ApplicationRecord
 
   def price
     @client = IEX::Api::Client.new(
-      publishable_token: ENV["IEX_API_PUBLISHABLE_TOKEN"],
-      secret_token: ENV["IEX_API_SECRET_TOKEN"],
-      endpoint: "https://cloud.iexapis.com/v1",
+      publishable_token: ENV['IEX_API_PUBLISHABLE_TOKEN'],
+      secret_token: ENV['IEX_API_SECRET_TOKEN'],
+      endpoint: 'https://cloud.iexapis.com/v1'
     )
     Rails.cache.fetch("#{cache_key_with_version}/price", expires_in: 12.hours) do
       @client.quote(code).latest_price
