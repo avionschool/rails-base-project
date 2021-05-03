b4ba95f3260f15749dada9186be404aaf0cf066b
Dave <ddcmendoza@gmail.com>
2021-05-03 14:41:55 +0800

    [HOTFIX] Added limiting on search.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,7 +2,9 @@ class StocksController < ApplicationController
   def index
     # @stocks = Stock.all
     @q = Stock.ransack(params[:q])
-    @stock_q = @q.result(distinct: true)
+    @length = ((@q.result.length)/10.0).ceil
+    @stock_q = @q.result(distinct: true).limit(10)
+    # @stock_q = params[:page] ?  @q.result(distinct: true).offset((params[:page].to_i - 1) * 10).limit(10) : @q.result(distinct: true).limit(10)
   end
 
   def show
