79ff6e8c9ee4ef0c0741567e99b36d55bf5c7840
AlexCitrus <kidong044@gmail.com>
2021-05-04 19:41:17 +0800

    [HOTFIX] Fixes timezone issue

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,8 @@ module RailsProject
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # Time zone
+    config.time_zone = "Beijing"
+    config.active_record.default_timezone = :local
   end
 end
