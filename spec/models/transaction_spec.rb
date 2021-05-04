98a1b73fb16109676061702b76f67bb729004e82
AlexCitrus <kidong044@gmail.com>
2021-05-04 14:49:17 +0800

    [WIP] Transaction Model Tests

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Transaction, type: :model do
   describe 'validations' do
     before do
-      described_class.create(volume: 100, price: 1.5, fulfilled: false, transaction_type: 'Sell')
+      described_class.create(volume: 0, price: 1.5, fulfilled: false, transaction_type: 'Buy')
     end
 
     it { expect(described_class.new).to validate_inclusion_of(:transaction_type).in_array(%w[Buy Sell]) }
