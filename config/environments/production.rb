d60516027013209a56db8a4af6950ec0d7b72e21
Dave <ddcmendoza@gmail.com>
2021-05-05 20:05:56 +0800

    [BUGFIX] Mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "rails_project_production"
-  config.action_mailer.default_url_options = { host: 'https://tradr-avion.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://tradr-avion.herokuapp.com' }
   config.action_mailer.perform_caching = false
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
