ce9fc1bd9f3a7874f4d1ea863589211690db3f40
Dave <ddcmendoza@gmail.com>
2021-05-05 20:15:07 +0800

    [HOTFIX] reset_password_token permit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :role_id, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :reset_password_token) }
   end
 end
 
