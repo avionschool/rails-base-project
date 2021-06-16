6ee8cc38cc7168f64c04219b25ae8c0f913d0e19
Rommel Torquator <rommeltorquator09@gmail.com>
2021-06-16 08:04:51 -0400

    Fixed rubocop syntax issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }  
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'signout', sign_up: 'register' }  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "home#index"
