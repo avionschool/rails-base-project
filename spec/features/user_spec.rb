ca7858a79ae39405533fd5ac1fade683d3186b4b
dlanoronald123 <dlanoronald123@gmail.com>
2021-06-16 21:52:59 +0800

    Add tests on user authentication

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -6,7 +6,7 @@ require 'rails_helper'
 #   #  visit new_user_session_path
 #   #  fill_in 'email_address', with: 'Sample Email'
 #   #  fill_in 'password', with: 'Sample Password'
-#   #  click_on 'Log In'
+#   #  click_on 'Log Ins'
 #   #  expect(page).to have_current_path(new_user_session_path)
 #   end
 # end
