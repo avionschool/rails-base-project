12604931d7f822096edc6953fe4c7348c40b0019
dlanoronald123 <dlanoronald123@gmail.com>
2021-06-16 21:12:28 +0800

    Add tests

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'rails', '~> 6.0.3', '>= 6.0.3.4'
 gem 'pg'
 gem 'puma', '~> 4.1'
 gem 'sass-rails', '>= 6'
-gem 'webpacker', '~> 4.0'
+gem 'webpacker'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,52 +1,42 @@
 require 'rails_helper'
 
-RSpec.feature "Authenticates", type: :feature do
-  describe "Register a user" do
-    it 'Successfull registration' do
+RSpec.describe 'Authenticates', type: :feature do
+  describe 'Register a user' do
+    it 'Register' do
       visit new_user_registration_path
-      fill_in 'first_name', with: 'Sample First Name'
-      fill_in 'last_name', with: 'Sample Last Name'
+      fill_in 'first_name', with: 'Sample'
+      fill_in 'last_name', with: 'Sample'
+      fill_in 'email_address', with: 'Sample@email.com'
+      fill_in 'password', with: 'Password'
+      fill_in 'confirm_password', with: 'Password'
+      click_on 'Sign Up'
+      expect(page).to have_content('Confirmation link has been send to your email. Kindly open to activate your account.')
+    end
+  end
+
+  describe 'Login user' do
+    it 'Redirect to index when valid' do
+      visit new_user_session_path
       fill_in 'email_address', with: 'Sample Email'
       fill_in 'password', with: 'Sample Password'
-      fill_in 'confirm_password', with: 'Sample Password'
+      click_on 'Log In'
+      expect(page).to have_current_path(new_user_session_path)
+    end
 
-      click_on 'Sign Up'
+    it 'Error message will appear if email is not valid' do
+      visit new_user_session_path
+      fill_in 'email_address', with: nil
+      fill_in 'password', with: 'Sample Password'
+      click_on 'Log In'
+      expect(page).to have_content('Invalid email.')
+    end
 
-      expect(page).to have_content("Confirmation link has been send to your email. Kindly open to activate your account.")
-    end     
-
-
-    describe "Login user" do
-      it 'Redirect to index when valid' do
-        visit new_user_session_path
-        fill_in 'email_address', with: 'Sample Email'
-        fill_in 'password', with: 'Sample Password'
-  
-        click_on 'Log In'
-  
-        expect(page).to have_current_path(new_user_session_path)
-      end   
-
-      it 'Error message will appear if email is not valid' do
-        visit new_user_session_path
-        fill_in 'email_address', with: nil
-        fill_in 'password', with: 'Sample Password'
-  
-        click_on 'Log In'
-  
-        expect(page).to have_content("Invalid email.")
-      end  
-      
-      
-      it 'Error message will appear if password is not valid' do
-        visit new_user_session_path
-        fill_in 'email_address', with: 'Sample Email'
-        fill_in 'password', with: nil
-  
-        click_on 'Log In'
-  
-        expect(page).to have_content("Invalid password.")
-      end  
+    it 'Error message will appear if password is not valid' do
+      visit new_user_session_path
+      fill_in 'email_address', with: 'Sample Email'
+      fill_in 'password', with: nil
+      click_on 'Log In'
+      expect(page).to have_content('Invalid password.')
     end
   end
 end
