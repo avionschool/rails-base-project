56ce2e021a9b99e6e6d72bd0a8ddba088a6c2e69
dlanoronald123 <dlanoronald123@gmail.com>
2021-06-17 19:09:59 +0800

    Add devise user model and tests

--- spec/requests/brokers_controllers_spec.rb
+++ spec/requests/brokers_controllers_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'BrokersControllers', type: :request do
   describe 'GET /brokers_controllers' do
-    it 'Redirect to broker main page' do
+    it 'Redirect to Broker main page' do
       get brokers_path
       expect(response).to have_http_status(:ok)
     end
