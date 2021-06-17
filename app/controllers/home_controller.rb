6eef61acdfcb0de6e05288dea35dd822750516c8
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-06-17 22:35:09 +0800

    fixed rubocop error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,11 @@
 class HomeController < ApplicationController
   def index
     @client = IEX::Api::Client.new(
-        publishable_token: ENV['IEX_API_PUBLISHABLE_TOKEN'],
-        secret_token: ENV['IEX_API_SECRET_TOKEN'],
-        endpoint: 'https://sandbox.iexapis.com/v1'
+      publishable_token: 'publishable_token',
+      secret_token: 'secret_token',
+      endpoint: 'https://cloud.iexapis.com/v1'
     )
 
     @quote = @client.quote('MSFT')
-    
   end
 end
