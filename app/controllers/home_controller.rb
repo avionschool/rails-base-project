e59257e5b6b009423768a901701f2558e1802aab
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-06-17 22:27:56 +0800

    added test for api calls

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,12 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @client = IEX::Api::Client.new(
+        publishable_token: ENV['IEX_API_PUBLISHABLE_TOKEN'],
+        secret_token: ENV['IEX_API_SECRET_TOKEN'],
+        endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+
+    @quote = @client.quote('MSFT')
+    
+  end
 end
