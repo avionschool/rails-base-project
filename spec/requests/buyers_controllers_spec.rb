dc956ae97b696a5508e9a6885635d13d62c18677
dlanoronald123 <dlanoronald123@gmail.com>
2021-06-17 21:30:11 +0800

    Add Controller tests on brokers and buyers

--- spec/requests/buyers_controllers_spec.rb
+++ spec/requests/buyers_controllers_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'BuyersControllers', type: :request do
   describe 'GET /buyers_controllers' do
     it 'Redirect to buyer main page' do
-      get buyers_path
+      get root_path
       expect(response).to have_http_status(:ok)
     end
   end
