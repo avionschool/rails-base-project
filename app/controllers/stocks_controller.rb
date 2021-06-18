08171e7bef0ed6eee776dd1ea2b696455e5962c9
Rommel Torquator <rommeltorquator09@gmail.com>
2021-06-17 21:30:10 -0400

    Implemented the requested changes

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -27,12 +27,8 @@ class StocksController < ApplicationController
 
   def fetch_api
     @client = IEX::Api::Client.new(
-      # publishable_token: ENV['PUBLIC'],
-      # secret_token: ENV['SECRET'],
-      publishable_token: 'Tpk_5e637bd957564d788d24f9b9491c1576',
-      secret_token: 'Tsk_81e8f3dbee154676a9ece271e45ac5e3',
-      # publishable_token: "Tpk_145ab4d03ae14ce8af5becf9da085fb3",
-      # secret_token: "Tsk_59788dd8ec49474090f3cf193f357a6c",
+      publishable_token: ENV['PUBLIC'],
+      secret_token: ENV['SECRET'],
       endpoint: 'https://sandbox.iexapis.com/v1' # 'https://cloud.iexapis.com/v1'
     )
   end
--- config/application.rb
+++ config/application.rb
@@ -14,11 +14,9 @@ require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module RailsProject
   class Application < Rails::Application
     config.before_configuration do
@@ -27,15 +25,12 @@ module RailsProject
         ENV[key.to_s] = value
       end if File.exists?(env_file)
     end
-    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
     # Don't generate system test files.
     config.generators.system_tests = nil    
   end
