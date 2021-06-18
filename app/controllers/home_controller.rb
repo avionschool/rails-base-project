11d1fa0a7f1bda25c04f45d0f2c8b75e9e08036c
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-06-18 08:24:16 +0800

    fixed rubocop issues not accepting direct read from ENV and global variables

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,20 @@
+require_relative '../custom_modules/.iex_token'
+
 class HomeController < ApplicationController
+  include IEXTokens
   def index
+    # @client = IEX::Api::Client.new(
+    #   publishable_token: ENV.fetch('IEX_API_PUBLISHABLE_TOKEN', false),
+    #   secret_token: ENV.fetch('IEX_API_SECRET_TOKEN', false),
+    #   endpoint: 'https://sandbox.iexapis.com/v1'
+    # )
+
     @client = IEX::Api::Client.new(
-      publishable_token: 'publishable_token',
-      secret_token: 'secret_token',
-      endpoint: 'https://cloud.iexapis.com/v1'
+      publishable_token: IEXTokens.iex_api_publishable_token,
+      secret_token: IEXTokens.iex_api_secret_token,
+      endpoint: 'https://sandbox.iexapis.com/v1'
     )
 
-    @quote = @client.quote('MSFT')
+    # @client = IEX::Api::Client.new
   end
 end
