5fab417e06f82b95941f1650b82831521d412eee
Leif Bersamina <leifbersamina@gmail.com>
2021-06-18 15:40:49 +0800

    generate admins_sessions_controller for admin login and logout

---
+++ app/controllers/admin_sessions_controller.rb
@@ -0,0 +1,19 @@
+class AdminSessionsController < ApplicationController
+  def new; end
+
+  def create
+    @admin = Admin.find_by(email: params[:email])
+
+    if @admin.present? && @admin.authenticate(params[:password])
+      session[:admin_id] = @admin.id
+      redirect_to admins_path, notice: 'Logged in successfully'
+    else
+      redirect_to admins_sign_in_path, alert: 'Invalid email or password'
+    end
+  end
+
+  def destroy
+    session[:admin_id] = nil
+    redirect_to admins_path, notice: 'Admin logged out'
+  end
+end
