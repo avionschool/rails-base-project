42be409115399ccf8d9ba54bd234a0d428ede982
Leif Bersamina <leifbersamina@gmail.com>
2021-06-18 17:07:04 +0800

    create helper methods for admin_sessions controller and admins controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   devise_group :user, contains: %i[buyer broker]
+  helper_method :current_admin
+  helper_method :admin_logged_in?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:type])
+  end
+
+  def current_admin
+    Admin.find_by(id: session[:admin_id])
+  end
+
+  def admin_logged_in?
+    !current_admin.nil?
   end
 end
