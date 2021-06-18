e5d159f3a9bea9ad3ace04e55d135f274cbc09be
Leif Bersamina <leifbersamina@gmail.com>
2021-06-18 20:37:40 +0800

    add approved column to user model

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_06_18_080832) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "type"
+    t.boolean "approved", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
