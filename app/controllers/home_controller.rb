11f963e9bb42973d118eaa14dc875fd2ce33ebff
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-06-19 09:16:23 +0800

    fixed rubocop offenses

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,13 @@ require_relative '../custom_modules/.iex_token'
 class HomeController < ApplicationController
   include IEXTokens
   def index
-    
-
     @client = IEX::Api::Client.new(
       publishable_token: IEXTokens.iex_api_publishable_token,
       secret_token: IEXTokens.iex_api_secret_token,
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
 
-    @symbols = @client.ref_data_symbols()
+    @symbols = @client.ref_data_symbols
 
     # @client = IEX::Api::Client.new
   end
