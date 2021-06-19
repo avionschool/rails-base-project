38a61f0dbcf33ef34f054cde9d61785af2726b6c
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-06-19 10:43:04 +0800

    added transact model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_19_021221) do
+ActiveRecord::Schema.define(version: 2021_06_19_023459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,16 @@ ActiveRecord::Schema.define(version: 2021_06_19_021221) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "symbol"
     t.string "companyname"
+  end
+
+  create_table "transacts", force: :cascade do |t|
+    t.integer "broker_id", null: false
+    t.integer "buyer_id", null: false
+    t.integer "stock_id", null: false
+    t.decimal "quantity"
+    t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
