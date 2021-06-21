899847480521465aec63a5b4a36fac28d020573c
AdrianIlaga <adrianilaga771@gmail.com>
2021-06-21 16:36:34 +0800

    added broker_stock relationship table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_064213) do
+ActiveRecord::Schema.define(version: 2021_06_21_072440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "broker_stocks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "stock_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["stock_id"], name: "index_broker_stocks_on_stock_id"
+    t.index ["user_id"], name: "index_broker_stocks_on_user_id"
+  end
 
   create_table "stocks", force: :cascade do |t|
     t.string "stock"
@@ -46,5 +55,7 @@ ActiveRecord::Schema.define(version: 2021_06_21_064213) do
     t.index ["user_type_id"], name: "index_users_on_user_type_id"
   end
 
+  add_foreign_key "broker_stocks", "stocks"
+  add_foreign_key "broker_stocks", "users"
   add_foreign_key "users", "user_types"
 end
