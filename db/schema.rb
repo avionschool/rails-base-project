70d786f525fda014b6cd2e233ba9ac39dd163bdb
AdrianIlaga <adrianilaga771@gmail.com>
2021-06-21 14:26:10 +0800

    added l_name, f_name, user_type and verified in users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_054605) do
+ActiveRecord::Schema.define(version: 2021_06_21_060149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,14 @@ ActiveRecord::Schema.define(version: 2021_06_21_054605) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name", limit: 30
+    t.string "last_name", limit: 30
+    t.bigint "user_type_id"
+    t.boolean "verified"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_type_id"], name: "index_users_on_user_type_id"
   end
 
+  add_foreign_key "users", "user_types"
 end
