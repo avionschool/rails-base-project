c5d46541079bb4083f23c4dc33eb2a2bd32c0c4e
AdrianIlaga <adrianilaga771@gmail.com>
2021-06-21 17:40:34 +0800

    added relationship between broker_user and stock

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,5 @@
 class Stock < ApplicationRecord
+  has_many :broker_stock, dependent: :destroy
+  # has_many :users, through: :broker_stock
+  has_many :brokers, -> { where(users: { user_types: [UserType.find_by(user_type: 'broker')] }) }, through: :broker_stock, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :user_types
+  has_many :broker_stock, dependent: :destroy
+  has_many :stocks, through: :broker_stock
 end
