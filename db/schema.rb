93f411a956042f006b705e454f315b9218e2cb41
AdrianIlaga <adrianilaga771@gmail.com>
2021-06-21 15:04:33 +0800

    added stocks table in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_060149) do
+ActiveRecord::Schema.define(version: 2021_06_21_064213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "stock"
+    t.string "ticker"
+    t.float "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "user_types", force: :cascade do |t|
     t.string "user_type"
