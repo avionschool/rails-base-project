7973152d70514c43570b4c3f87cf3753490e6440
AdrianIlaga <adrianilaga771@gmail.com>
2021-06-22 18:12:43 +0800

    corrected brokers relation in stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,5 @@
 class Stock < ApplicationRecord
   has_many :broker_stock, dependent: :destroy
-  # has_many :users, through: :broker_stock
-  has_many :brokers, -> { where(users: { user_types: [UserType.find_by(user_type: 'broker')] }) }, through: :broker_stock, source: :user
+  # has_many :users, through: :broker_stock --> Disabled to prevent stocks from calling users who are not brokers
+  has_many :brokers, -> { where(users: { user_type_id: UserType.find_by(user_type: 'broker').id }) }, through: :broker_stock, source: :user
 end
