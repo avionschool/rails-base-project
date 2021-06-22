adcd19f93c6398999c1caa0d986fbf1fd308bb7f
Leif Bersamina <leifbersamina@gmail.com>
2021-06-22 20:36:16 +0800

    associate the user_transaction to broker_stock and buyer model

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,5 +1,8 @@
 class Buyer < User
   has_many :buyer_stocks, dependent: :destroy
+  has_many :user_transactions, dependent: :destroy
+  has_many :broker_stocks, through: :user_transactions
+
   before_create :set_default_to_true
 
   def set_default_to_true
