d1f8a1e6c228ac190e38250020b853f0265a99a2
AdrianIlaga <adrianilaga771@gmail.com>
2021-06-23 14:39:24 +0800

    Added pending tests for validation and scope to user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when testing validation' do
+    it 'ensures there is first name'
+
+    it 'ensures there is last name'
+
+    it 'ensures there is email'
+
+    it 'ensures email is unique'
+
+    it 'ensures there is password'
+
+    it 'ensures there is user type id'
+
+    it 'ensures verified is not nil'
+
+    it 'should save properly'
+  end
+
+  context 'when testing scopes' do
+    it 'should only get buyers'
+
+    it 'should only get brokers'
+
+    it 'should only get admin'
+  end
 end
