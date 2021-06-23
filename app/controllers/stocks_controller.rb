4a457b0eefdf260e618e58b17a46f983401066b5
Leo <leocsdev@gmail.com>
2021-06-23 14:35:30 +0800

    Replaced tokens with ENV variables.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,10 +1,11 @@
 class StocksController < ApplicationController
   def index
     @iex_client = IEX::Api::Client.new(
-      publishable_token: 'Tpk_1b73e94d5c7a45839071c3c6fa651dbe', 
-      secret_token: 'Tsk_3f7e77a8a28142a3b816b9ea86c1efea', 
+      publishable_token: ENV['IEX_PUBLISHABLE_TOKEN'],
+      secret_token: ENV['IEX_SECRET_TOKEN'],
       endpoint: 'https://sandbox.iexapis.com/v1'
-      # endpoint: 'https://cloud.iexapis.com/v1' # use 'https://sandbox.iexapis.com/v1' for Sandbox
+      # endpoint: 'https://cloud.iexapis.com/v1' 
+      # use 'https://sandbox.iexapis.com/v1' for Sandbox
     ).stock_market_list(:mostactive)
   end
 end
