1e214729c4996084dca264b0b2b06a7df1073f7f
santosral <sralphian029@gmail.com>
2021-06-23 16:33:32 +0800

    Removed Roles Table

--- db/migrate/20210622125015_create_roles.rb
+++
@@ -1,9 +0,0 @@
-class CreateRoles < ActiveRecord::Migration[6.0]
-  def change
-    create_table :roles do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_22_141056) do
+ActiveRecord::Schema.define(version: 2021_06_23_083140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
