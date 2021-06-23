0e3feb0b7d593f31cec23e139e9a7e7f9a01a0a0
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-06-23 14:54:57 +0800

    added username upon user registration, and default paramater value for :approved when new user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         
+  before_save :default_approved
+  def default_approved
+    if self.type != 'Broker'
+        self.approved ||= true
+    else
+        self.approved ||= false
+    end
+  end
+
 end
