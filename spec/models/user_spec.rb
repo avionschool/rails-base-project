bc00287b81d8419cee45ee8feb4c3e45d26c7d02
AdrianIlaga <adrianilaga771@gmail.com>
2021-06-23 16:43:31 +0800

    Added test user data in user_spec validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let!(:user) { described_class.new }
+  let(:admin) { UserType.find_by(user_type: 'admin') }
+  let(:buyer) { UserType.find_by(user_type: 'buyer') }
+  let(:broker) { UserType.find_by(user_type: 'broker') }
+
   context 'when testing validation' do
+    before do
+      user.email = 'test@gmail.com'
+      user.password = 'password'
+      user.password_confirmation = 'password'
+      user.first_name = 'Firstname'
+      user.last_name = 'Lastname'
+      user.user_type_id = buyer.id
+      user.verified = true
+    end
+
     it 'ensures there is first name'
 
     it 'ensures there is last name'
