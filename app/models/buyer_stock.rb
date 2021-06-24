e02c5a3d4af6bf80bbb18a1840e3e6be3fcb5c9f
dlanoronald123 <dlanoronald123@gmail.com>
2021-06-24 16:38:03 +0800

    updated buyer_stock model

--- app/models/buyer_stock.rb
+++ app/models/buyer_stock.rb
@@ -1,9 +1,5 @@
 class BuyerStock < ApplicationRecord
-<<<<<<< HEAD
   belongs_to :buyer
-
-=======
->>>>>>> Update broker and buyer stock validations
   validates :name, presence: true
   validates :quantity, presence: true
   validates :price, presence: true
