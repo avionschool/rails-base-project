72fc671e13deeb8e98566530c3948a66a7b2f33e
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-06-25 00:57:48 +0800

    generated user factory bot

---
+++ test/factories/users.rb
@@ -0,0 +1,26 @@
+FactoryBot.define do
+  factory :user do
+    username { 'user' }
+    email { "#{username}@email.com" }
+    password { 'secure123' }
+
+    trait :admin do
+      username { 'admin' }
+      role { 'admin' }
+    end
+
+    trait :buyer do
+      username { 'buyer' }
+      role { 'buyer' }
+    end
+
+    trait :broker do
+      username { 'broker' }
+      role { 'broker' }
+    end
+
+    factory :admin_user, traits: [:admin]
+    factory :buyer_user, traits: [:buyer]
+    factory :broker_user, traits: [:broker]
+  end
+end
