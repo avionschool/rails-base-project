3a9146849b51f52a5b0ade571083ba16c532240f
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-06-24 19:30:22 +0800

    added api tokens to credentials

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,10 @@
-require_relative '../custom_modules/.iex_token'
-
 class HomeController < ApplicationController
-  include IEXTokens
   before_action :authenticate_user!
 
   def index
     @client = IEX::Api::Client.new(
-      publishable_token: IEXTokens.iex_api_publishable_token,
-      secret_token: IEXTokens.iex_api_secret_token,
+      publishable_token: Rails.application.credentials.iex_publishable_token,
+      secret_token: Rails.application.credentials.iex_secret_token,
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
 
