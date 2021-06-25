9732809421a285c1a32e01179667066b0ffa26eb
EliezerAmbion <eliezerambion@gmail.com>
2021-06-25 17:07:45 +0800

    revision from rubocop errors

--- spec/requests/buyer_stocks_controllers_spec.rb
+++ spec/requests/buyer_stocks_controllers_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "BuyerStocksControllers", type: :request do
-  describe "GET /buyer_stocks" do
-    it "should get index" do
+RSpec.describe 'BuyerStocksControllers', type: :request do
+  describe 'GET /buyer_stocks' do
+    it 'gets index' do
       get buyer_stocks_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
   end
 end
