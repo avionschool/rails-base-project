a95073e6d00100cd93a698310e2965ec365ec315
AdrianIlaga <adrianilaga771@gmail.com>
2021-06-25 16:34:18 +0800

    integrated IEX API to stock database

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,27 @@
 class PagesController < ApplicationController
+  before_action :update_stocks
+
   def home
-    @stocks_list = Stock.most_active_stocks
+    @stocks_list = Stock.all
+  end
+
+  private
+
+  def update_stocks
+    stocks_list = Stock.most_active_stocks
+    stocks_list.each do |stock_data|
+      stock = Stock.find_or_create_by(ticker: stock_data.symbol) do |s|
+        s.price = stock_data.latest_price
+        s.stock = stock_data.company_name
+      end
+      if stock.price != stock_data.latest_price
+        stock.price = stock_data.latest_price
+        stock.save
+      end
+    end
+  end
+
+  def stock_params
+    params.require(:stock).permit(:stock, :price, :ticker)
   end
 end
