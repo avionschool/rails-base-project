1140a087dc72ec0cbae6d8af5030734521a5d680
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-06-25 20:37:17 +0800

    adding navbar for sign_up sign_in and home page and side navbar for other pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def current_uri
+    request.env['PATH_INFO']
+  end
+
+  def top_nav_bar?
+    return true if current_uri == '/users/sign_up' || current_uri == '/' || current_uri == '/users/sign_in'
+  end
 end
