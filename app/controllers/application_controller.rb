58b540e7f9a59beebed1e6692800adc0af2f5cd0
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-06-25 23:10:05 +0800

    generate cancan ability and create authorization rule on userStock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
 
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = exception.message
+    redirect_to root_url
+  end
+
   protected
 
   def update_allowed_parameters
--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,6 +1,7 @@
 class UserStocksController < ApplicationController
-    def my_portfolio
-    end
-    def transactions
-    end
+  load_and_authorize_resource
+
+  def my_portfolio; end
+
+  def transactions; end
 end
