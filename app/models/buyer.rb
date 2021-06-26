fda6ca0104dfd4db7c4ecb5d98fd380323885cc5
Leif Bersamina <leifbersamina@gmail.com>
2021-06-26 11:03:36 +0800

    transfer validates password_confirmation from the user model to the buyer model

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -3,6 +3,8 @@ class Buyer < User
   has_many :user_transactions, dependent: :destroy
   has_many :broker_stocks, through: :user_transactions
 
+  validates :password_confirmation, presence: true
+
   before_create :set_default_to_true
 
   def set_default_to_true
