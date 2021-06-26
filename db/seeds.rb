071d79801f15c9fd3b082c1cba1c788924766ec5
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-06-26 13:23:24 +0800

    seeded all stocks symbols in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+    @client = IEX::Api::Client.new(
+        publishable_token: Rails.application.credentials.iex_publishable_token,
+        secret_token: Rails.application.credentials.iex_secret_token,
+        endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+
+    @symbols = @client.ref_data_symbols
+    @symbols.each do |x|
+        Stock.create(symbol: x.symbol)
+    end
+    
