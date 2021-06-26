4cada769f4a69ad7aeaebc02cfc48a891394f2b0
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-06-26 12:37:20 +0800

    generate stocks views and modify navbar links

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  get 'stock/search', to: 'stock#search'
+  get 'stock/search', to: 'stocks#search'
   get 'transactions', to:'user_stocks#transactions'
   get 'my_portfolio', to:'user_stocks#my_portfolio'
   get 'marketplace', to: 'stocks#index', as: 'marketplace'
 end
+
---
+++ spec/requests/stocks_controllers_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'StocksControllers', type: :request do
+  let(:user) { create(:user) }
+
+  describe 'GET marketplace' do
+    it 'returns a redirect response not logged_in' do
+      get marketplace_path
+      expect(response).to have_http_status(:found)
+    end
+
+    it 'returns a success response if logged_in' do
+      sign_in user
+      get marketplace_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'GET stock_search' do
+    it 'returns a redirect response not logged_in' do
+      get stock_search_path
+      expect(response).to have_http_status(:found)
+    end
+
+    it 'returns a success response if logged_in' do
+      sign_in user
+      get stock_search_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
