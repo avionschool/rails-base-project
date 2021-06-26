15e42b0a7b6f24cbef9014448e62961edebbee84
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-06-26 11:22:42 +0800

    enabled removing of stock from broker's portfolio

--- app/controllers/broker_stocks_controller.rb
+++ app/controllers/broker_stocks_controller.rb
@@ -12,6 +12,20 @@ class BrokerStocksController < ApplicationController
     end
   end
 
+  def update
+    @broker_stock = BrokerStock.find(params[:id])
+    if @broker_stock.update(broker_stock_params)
+        redirect_to root_path, notice: 'Stock was updated successfully.'
+    end
+  end
+
+  def destroy
+    @broker_stock = BrokerStock.find(params[:id])
+    @broker_stock.destroy
+    redirect_to root_path, notice: 'Stock was removed from portfolio.'
+  end
+
+ 
   def broker_stock_params
     params.require(:broker_stock).permit(:user_id, :stock_id, :companyname, :quantity, :price)
   end
