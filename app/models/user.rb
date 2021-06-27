ba9522cb78b7674c63b3cb97be0b4764927cb900
AdrianIlaga <72000424+AdrianIlaga@users.noreply.github.com>
2021-06-27 19:56:15 +0800

    Edit user password validation to be on create only

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,8 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, confirmation: true
-  validates :password_confirmation, presence: true
+  validates :password, confirmation: true, presence: true, on: :create
+  validates :password_confirmation, presence: true, on: :create
   validates :user_type_id, presence: true
   validates_associated :user_type
   validates :verified, inclusion: [true, false]
