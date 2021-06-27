4cd19186ddc1ec8f57a3024626d5ebd24f957a33
Leif Bersamina <leifbersamina@gmail.com>
2021-06-27 20:55:19 +0800

    add test for buyer_stock show and change post test

--- app/controllers/buyer_stocks_controller.rb
+++ app/controllers/buyer_stocks_controller.rb
@@ -1,3 +1,28 @@
 class BuyerStocksController < ApplicationController
-  def index; end
+  before_action :authenticate_buyer!, exept: :show
+
+  def index
+    @buyer_stocks = current_buyer.buyer_stocks
+  end
+
+  def show
+    @buyer_stock = current_buyer.buyer_stocks.find(params[:id])
+  end
+
+  def create
+    @buyer_stock = current_buyer.user_transactions.build(buyer_stock_params)
+
+    if @buyer_stock.save
+      current_buyer.buyer_stocks.create(symbol: @buyer_stock.broker_stock.symbol, company_name: @buyer_stock.broker_stock.company_name)
+      redirect_to buyer_stocks_path, notice: "Successfully #{@buyer_stock.broker_stock.symbol}"
+    else
+      redirect_to broker_stocks_path, alert: @buyer_stock.errors.full_messages.first
+    end
+  end
+
+  private
+
+  def buyer_stock_params
+    params.require(:buyer_stock).permit(:broker_stock_id)
+  end
 end
