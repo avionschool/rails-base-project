d6e6fb97720e298147583f0652c47bf0322d5325
AdrianIlaga <adrianilaga771@gmail.com>
2021-06-27 20:02:08 +0800

    edited app_controller to abide with rubocop standards

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
-  before_action :get_user_types, if: :devise_controller?
+  before_action :user_types, if: :devise_controller?
 
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :user_type_id)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :user_type_id) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password) }
   end
 
-  def get_user_types
+  def user_types
     @broker = UserType.find_by(user_type: 'broker')
     @buyer = UserType.find_by(user_type: 'buyer')
     @admin = UserType.find_by(user_type: 'admin')
