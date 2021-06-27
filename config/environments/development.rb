d94f4e9579f251881a67bedf8109d3350e47be41
Eunicegenel <eunel_soliveres@yahoo.com>
2021-06-27 20:27:30 +0800

    heroku deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,9 +65,12 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp@gmail.com",
     :port                 => 587,
-    :user_name            => ENV['gmail_username'],
-    :password             => ENV['gmail_password'],
+    :user_name            => 'eunel.soliveres@gmail.com',
+    :password             => 'Eunicegenel2004',
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
 end
+
+# :user_name            => ENV['gmail_username'],
+# :password             => ENV['gmail_password'],
