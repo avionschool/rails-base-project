59141f3d42aa8c72585fd51efa1ac7c7aa26dff7
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-06-28 17:00:44 +0800

    admin user control, routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  scope "/admin" do
+    resources :users
+  end
+  get "/admin/home", to: 'users#admin_home', as: 'admin_home'
+  get "/admin/pending_users", to: 'users#pending_users', as: 'pending_user'
+  post "/admin/:id/pending_users", to: 'users#approve_pending', as: 'approve_pending_user'
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'stock/search', to: 'stocks#search'
