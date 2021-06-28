5b44bb6581e3cf168bb1b82cdd1e2da7a2f8f34d
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-06-28 09:06:04 +0800

    Buyer and Broker Portfolio Page

--- app/models/user_stock.rb
+++ app/models/user_stock.rb
@@ -1,10 +1,24 @@
 class UserStock < ApplicationRecord
   belongs_to :user
   belongs_to :stock
-
+  scope :brokerstocks, lambda {
+    joins(:user).where(users: { role: 'broker' })
+  }
   validates :average_price, numericality: { greater_than: 0 }, if: :user_buyer?
   validates :total_shares, numericality: { greater_than: 0 }, if: :user_buyer?
   def user_buyer?
     self&.user&.buyer?
+  end
+  def market_value
+    '%.2f' % (self.total_shares * self.stock.last_price.to_f)
+  end
+  def net_cost
+    '%.2f' % (self.total_shares * self.average_price.to_f)
+  end 
+  def gain_loss
+    '%.2f' % (self.market_value.to_f - self.net_cost.to_f)
+  end
+  def percent_gain_loss
+    '%.2f' % ((self.gain_loss.to_f/self.net_cost.to_f)*100)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'stock/search', to: 'stocks#search'
   get 'transactions', to:'user_stocks#transactions'
   get 'my_portfolio', to:'user_stocks#my_portfolio'
   get 'marketplace', to: 'stocks#index', as: 'marketplace'
-  resources :user_stocks, only: [:create, :destroy,:new]
+  resources :user_stocks, only: [:create, :destroy]
+
+  get 'new_order(/:id)', to: 'user_stocks#new', as: 'new_user_stock'
+  post 'stocks/order', to: 'user_stocks#order', as: 'order_stocks'
+
 
 end
 
