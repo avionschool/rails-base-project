ea4f33924ba152e8b4bf54a1a79eac8f82d8ea85
AdrianIlaga <72000424+AdrianIlaga@users.noreply.github.com>
2021-06-28 17:25:18 +0800

    added users, pending, and portfolio methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,22 @@ class PagesController < ApplicationController
   def home
     @name = current_user.first_name
   end
+  # Admin Only
+  def users
+    @buyers = User.buyers.all
+    @brokers = User.brokers.where(verified: true)
+    @users = User.all
+  end
+
+  def pending
+    @pending = User.brokers.where(verified: false)
+  end
+  # Brokers and Buyers Only
+  def portfolio
+    if @user_type == 'broker'
+      @portfolio = current_user.stocks
+    elsif @user_type == 'buyer'
+      @portfolio = curent_user.buyer_stocks
+    end
+  end
 end
