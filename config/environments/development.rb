79e7a8b22374ded8d90428b35e7387feaef36be1
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-06-28 09:25:42 +0800

    installing and configuring mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
 
   # Print deprecation notices to the Rails logger.
@@ -62,4 +61,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
 end
