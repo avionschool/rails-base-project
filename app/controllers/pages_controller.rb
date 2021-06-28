b17cc5fc65ce810ac9a676cd40ec6ab21008f1f4
AdrianIlaga <72000424+AdrianIlaga@users.noreply.github.com>
2021-06-28 14:06:05 +0800

    added name and usertype vars to home method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,30 +1,8 @@
 class PagesController < ApplicationController
-  before_action :update_stocks
   before_action :authenticate_user!
 
   def home
-    @stocks_list = Stock.all
-  end
-
-  private
-
-  def update_stocks
-    stocks_list = Stock.most_active_stocks
-    return if stocks_list.nil?
-
-    stocks_list.each do |stock_data|
-      stock = Stock.find_or_create_by(ticker: stock_data.symbol) do |s|
-        s.price = stock_data.latest_price
-        s.stock = stock_data.company_name
-      end
-      if stock.price != stock_data.latest_price
-        stock.price = stock_data.latest_price
-        stock.save
-      end
-    end
-  end
-
-  def stock_params
-    params.require(:stock).permit(:stock, :price, :ticker)
+    @user_type = current_user.user_type.user_type
+    @name = current_user.first_name
   end
 end
