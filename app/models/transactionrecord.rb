d1e793bbdff3b4274745c3575820234f7b806e62
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-06-28 09:06:34 +0800

    buyer and broker transaction page

--- app/models/transactionrecord.rb
+++ app/models/transactionrecord.rb
@@ -1,5 +1,5 @@
 class Transactionrecord < ApplicationRecord
-  enum transactiontype: { buy: 0, sell: 1, add: 2, remove: 3 }
+  enum transaction_type: { buy: 0, sell: 1, add: 2, remove: 3 }
   belongs_to :broker, class_name: 'User'
   belongs_to :buyer, class_name: 'User'
   belongs_to :stock
