a6e1b6492d63d34fcbd790fbd59614baa4578aca
AdrianIlaga <72000424+AdrianIlaga@users.noreply.github.com>
2021-06-28 17:24:31 +0800

    added addt'l routes for pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # Home
   root to: "pages#home"
+  # Users
+  devise_for :users
+  # Admin Only
+  get '/admin/users' => 'pages#users', as: 'users_index'
+  get '/admin/pending' => 'pages#pending', as: 'users_pending'
+  # Buyer or Broker Only
+  get '/portfolio' => 'pages#portfolio', as: 'pages_portfolio'
+  # All
+  get '/transactions' => 'pages#transactions', as: 'pages_transactions'
+  # Stocks
   resources :stocks, only: [:index]
 end
