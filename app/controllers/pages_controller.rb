f3512636a5f8f2a4f3ff6fa67e2a0e652b50e07a
AdrianIlaga <72000424+AdrianIlaga@users.noreply.github.com>
2021-06-28 21:09:09 +0800

    added admin approval of broker

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -22,7 +22,7 @@ class PagesController < ApplicationController
 
   def approve_pending
     @user = User.find(params[:id])
-    @user.update(:user_params)
+    redirect_to users_pending_path if @user.update(user_params)
   end
 
   # Brokers and Buyers Only
@@ -31,13 +31,13 @@ class PagesController < ApplicationController
     when 'broker'
       @portfolio = current_user.stocks
     when 'buyer'
-      @portfolio = curent_user.buyer_stocks
+      @portfolio = current_user.buyer_stocks
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :user_type_id)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :user_type_id, :verified)
   end
 end
