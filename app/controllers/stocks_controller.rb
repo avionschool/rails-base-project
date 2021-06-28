93c173d6a2743aac18fba0a501b7a3f363bbfdc2
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-06-28 09:05:12 +0800

    buyer market place and new_order page plus controller that will create, update or delete buyer user_stocks

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,10 @@
 class StocksController < ApplicationController
   def index
-    @stocks = Stock.paginate(page: params[:page], per_page: 10)
+    @stocks = if current_user.broker?
+                Stock.paginate(page: params[:page], per_page: 10)
+              else
+                UserStock.brokerstocks.paginate(page: params[:page], per_page: 10)
+              end
   end
 
   def search; end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   enum role: { broker: 0, buyer: 1, admin: 2 }
   has_many :user_stocks, dependent: :destroy
   has_many :stocks, through: :user_stocks, dependent: :nullify
-  has_many :broker_transactionrecords, class_name: 'Transactionrecord', foreign_key: 'broker_id', inverse_of: 'transactionrecord', dependent: :nullify
-  has_many :buyer_transactionrecords, class_name: 'Transactionrecord', foreign_key: 'buyer_id', inverse_of: 'transactionrecord', dependent: :nullify
+  has_many :broker_transactionrecords, class_name: 'Transactionrecord', foreign_key: 'broker_id', dependent: :nullify
+  has_many :buyer_transactionrecords, class_name: 'Transactionrecord', foreign_key: 'buyer_id', dependent: :nullify
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -16,5 +16,8 @@ class User < ApplicationRecord
     return false unless stock
 
     stocks.exists?(id: stock.id)
+  end
+  def total_net_cost
+    self.user_stocks.sum("total_shares * average_price")
   end
 end
