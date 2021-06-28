23c2a60ee27ea62ba12eee482e12c7e2cebc90d0
dlanoronald123 <dlanoronald123@gmail.com>
2021-06-28 16:02:30 +0800

    Modified admin index

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -24,9 +24,49 @@ class AdminsController < ApplicationController
     redirect_to root_path, notice: 'Admin account was deleted successfully.'
   end
 
+  def new_broker
+    @broker = Broker.new
+  end
+
+  def new_buyer
+    @buyer = Buyer.new
+  end
+
+  def create_new_broker
+    @broker = Broker.new(broker_params)
+    @broker.admin_id = Admin.first.id
+    if @broker.save
+      redirect_to admin_users_path, notice: 'Broker created successfully.'
+    else
+      flash[:alert] = @broker.errors.full_messages.to_sentence
+      render action: 'new_broker'
+    end
+  end
+
+  def create_new_buyer
+    @buyer = Buyer.new(buyer_params)
+    @buyer.admin_id = Admin.first.id
+
+    if @buyer.save
+      redirect_to admin_users_path, notice: 'Buyer created successfully.'
+    else
+      flash[:alert] = @buyer.errors.full_messages.to_sentence
+      render action: 'new_buyer'
+    end
+  end
+
   private
 
   def admin_params
     params.require(:admin).permit(:email, :username, :approved, :confirmable, :type)
   end
+
+  def broker_params
+    params.require(:broker).permit(:email, :username, :approved, :confirmable, :type)
+  end
+
+  def buyer_params
+    params.require(:buyer).permit(:email, :username, :approved, :confirmable, :type)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   resources :buyers, :brokers, :admins, :users, :broker_stocks, :stocks
 
   root 'home#index'
+
+   # Admin: Create new accounts
+   get '/admins/new_broker' => 'admins#new_broker'
+   get '/admins/new_buyer' => 'admins#new_buyer'
+   post '/admins/new_broker' => 'admins#create_new_broker'
+   post '/admins/new_buyer' => 'admins#create_new_buyer'
   
 end
