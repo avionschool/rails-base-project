d1d47631316d04423d40e788427981f5c2b25636
AdrianIlaga <72000424+AdrianIlaga@users.noreply.github.com>
2021-06-28 14:04:46 +0800

    moved stocks method to stocks controller.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,2 +1,30 @@
 class StocksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :update_stocks
+
+  def index
+    @stocks_list = Stock.all
+  end
+
+  private
+
+  def update_stocks
+    stocks_list = Stock.most_active_stocks
+    return if stocks_list.nil?
+
+    stocks_list.each do |stock_data|
+      stock = Stock.find_or_create_by(ticker: stock_data.symbol) do |s|
+        s.price = stock_data.latest_price
+        s.stock = stock_data.company_name
+      end
+      if stock.price != stock_data.latest_price
+        stock.price = stock_data.latest_price
+        stock.save
+      end
+    end
+  end
+
+  def stock_params
+    params.require(:stock).permit(:stock, :price, :ticker)
+  end
 end
