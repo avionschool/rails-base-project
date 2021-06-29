46fe9075c4193ff5916f2cc40e1ec00ac29a95ac
Leif Bersamina <leifbersamina@gmail.com>
2021-06-29 16:16:58 +0800

    add quantity and total_price column on user_transaction table

--- db/migrate/20210622121417_create_user_transactions.rb
+++ db/migrate/20210622121417_create_user_transactions.rb
@@ -3,6 +3,8 @@ class CreateUserTransactions < ActiveRecord::Migration[6.0]
     create_table :user_transactions do |t|
       t.belongs_to :buyer
       t.belongs_to :broker_stock
+      t.integer :quantity, default: 1
+      t.float :total_price
       t.timestamps
     end
   end
