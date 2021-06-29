37a30e3fff0eaf539b8cdb9675956d7adad2bdda
Leo <leocsdev@gmail.com>
2021-06-29 11:06:50 +0800

    Disabled authenticate user to show homepage even no one is logged in.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def home
-    @name = current_user.first_name
+    if current_user
+      @name = current_user.first_name
+    end
   end
 
   # Admin Only
