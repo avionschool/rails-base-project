9bf42b9baa16cdb9cf504dce6f5569b2b9ab179c
AdrianIlaga <72000424+AdrianIlaga@users.noreply.github.com>
2021-06-30 16:09:08 +0800

    added uniqueness with scope validation

--- app/models/broker_stock.rb
+++ app/models/broker_stock.rb
@@ -3,4 +3,6 @@ class BrokerStock < ApplicationRecord
   belongs_to :stock
   has_many :transactions, dependent: :destroy
   has_many :buyers, -> { where(users: { user_type_id: UserType.find_by(user_type: 'buyer').id }) }, through: :transactions, source: :user
+  # Validations
+  validates :stock_id, uniqueness: {scope: :user_id}
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,6 @@ class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :broker_stock
   has_many :transaction_logs, dependent: :destroy
+  # Validations
+  validates :broker_stock_id, uniqueness: {scope: :user_id}
 end
