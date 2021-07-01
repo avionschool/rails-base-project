98173e1bc8a90b2b4ead57bff996ec76e9f49b6f
AdrianIlaga <adrianilaga771@gmail.com>
2021-07-01 13:41:09 +0800

    corrected adminn_controller to abide with rubocop

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,5 @@
 class AdminController < ApplicationController
-
-  before_action :user_types, only: [:new, :edit]
+  before_action :user_types, only: %i[new edit]
 
   def users
     @buyers = User.buyers.all
@@ -34,7 +33,7 @@ class AdminController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to users_index_path, notice: "User Successfuly Created"
+      redirect_to users_index_path, notice: 'User Successfuly Created'
     else
       render :new
     end
@@ -47,11 +46,10 @@ class AdminController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_index_path, notice: "User Successfuly Edited"
+      redirect_to users_index_path, notice: 'User Successfuly Edited'
     else
       render :edit
     end
-
   end
 
   private
@@ -61,7 +59,7 @@ class AdminController < ApplicationController
     @buyer = UserType.find_by(user_type: 'buyer')
     @admin = UserType.find_by(user_type: 'admin')
   end
-  
+
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :user_type_id, :verified)
   end
