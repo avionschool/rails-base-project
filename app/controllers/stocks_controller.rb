51ef09269a626036adcc336b2e7b57fb3fd99c75
Eunicegenel <eunel_soliveres@yahoo.com>
2021-07-01 18:25:42 +0800

    added buying transaction for users, added creation or updating of user stocks upon buying

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,6 +5,7 @@ class StocksController < ApplicationController
 
   def search
     @stock = Stock.new_lookup(params[:ticker])
+    @user = User.find_by(id: session[:user_id])
     render 'show'
   end
 
@@ -27,5 +28,39 @@ class StocksController < ApplicationController
       # exception
     end
     redirect_to stocks_path
+  end
+
+  def transaction_buy
+    @transaction = Transaction.new(transaction_params)
+    @user = User.find_by(id: params[:user_id])
+    @buyer_stock = BuyerStock.find_by(user_id: @user.id, ticker: @transaction.ticker)
+    if @transaction.save
+      @total_price = @transaction.price * @transaction.quantity
+      @transaction.update(total_price: @total_price.round(2))
+      if @buyer_stock == nil 
+        BuyerStock.create(stock_params)
+        BuyerStock.update(total_price: @total_price)
+        redirect_to stocks_path
+      else
+        @stock = BuyerStock.new(stock_params)
+        @new_qty = @buyer_stock.quantity + @transaction.quantity
+        @new_total_price = (@new_qty * @transaction.price).round(2)
+        @new_percent = @stock.change_percent
+        @new_price = @transaction.price
+        BuyerStock.update(quantity: @new_qty, total_price: @new_total_price, change_percent: @new_percent, price: @new_price)
+        redirect_to stocks_path
+      end
+    else
+    end
+  end
+
+  private
+
+  def transaction_params
+    params.require(:stock).permit(:company, :price, :ticker, :quantity, :buyer, :buyer_id, :seller)
+  end
+
+  def stock_params
+    params.require(:stock).permit(:company, :price, :ticker, :quantity, :user_id, :change_percent)
   end
 end
