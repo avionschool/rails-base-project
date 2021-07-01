6972ced126a97a85279dc149051ff4226910f10c
lnabrigo18 <lnabrigo@gmail.com>
2021-07-01 12:22:35 +0800

    Changes in association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  has_many :user_stocks
-  has_many :stocks, through: :user_stocks
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
   validates :role, presence: true
