9b915e960968b89e3d9b27574d53e588bc95448e
AdrianIlaga <adrianilaga771@gmail.com>
2021-07-01 20:51:40 +0800

    added all changes rubocop requested

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -27,13 +27,13 @@ class PagesController < ApplicationController
     @transaction = Transaction.find(params[:id])
     @purchased_stock = params[:transaction][:purchased_stock].to_i
     @transaction.amount = @transaction.amount + @purchased_stock
-    if @transaction.save
-      @ts = TransactionLog.new
-      @ts.transaction_id = params[:id]
-      @ts.amount_change = @purchased_stock
-      @ts.save
-      redirect_to pages_portfolio_path, notice: 'Stock Successfully Purchased'
-    end
+    return unless @transaction.save
+
+    @ts = TransactionLog.new
+    @ts.transaction_id = params[:id]
+    @ts.amount_change = @purchased_stock
+    @ts.save
+    redirect_to pages_portfolio_path, notice: 'Stock Successfully Purchased'
   end
 
   def sell
@@ -44,15 +44,16 @@ class PagesController < ApplicationController
     @transaction = Transaction.find(params[:id])
     @sold_stock = params[:transaction][:sold_stock].to_i
     return unless @sold_stock <= @transaction.amount
+
     @sold_stock = 0 - @sold_stock
     @transaction.amount = @transaction.amount + @sold_stock
-    if @transaction.save
-      @ts = TransactionLog.new
-      @ts.transaction_id = params[:id]
-      @ts.amount_change = @sold_stock
-      @ts.save
-      redirect_to pages_portfolio_path, notice: 'Stock Successfully Purchased'
-    end
+    return unless @transaction.save
+
+    @ts = TransactionLog.new
+    @ts.transaction_id = params[:id]
+    @ts.amount_change = @sold_stock
+    @ts.save
+    redirect_to pages_portfolio_path, notice: 'Stock Successfully Purchased'
   end
 
   private
