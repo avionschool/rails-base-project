cb6a4dfe258f6fc0e9a28ba99e5ec72c0d0464df
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-07-02 14:13:05 +0800

    commenting out seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
- client = Stock.sandbox_api
- iex_client = Stock.iex_api
- symbols = client.ref_data_symbols()
- nas_symbols = symbols.select{|symbol| symbol.exchange == 'NAS' }
- selected_symbols = (0... nas_symbols.length).select{ |x| x%5 == 5-1 }.map { |y| nas_symbols[y] } 
+#  client = Stock.sandbox_api
+#  iex_client = Stock.iex_api
+#  symbols = client.ref_data_symbols()
+#  nas_symbols = symbols.select{|symbol| symbol.exchange == 'NAS' }
+#  selected_symbols = (0... nas_symbols.length).select{ |x| x%5 == 5-1 }.map { |y| nas_symbols[y] } 
 
-selected_symbols.each do|stock| 
-  begin
-   Stock.find_or_create_by('ticker'=>stock.symbol, 'name' => client.company(stock.symbol).company_name,'last_price'=>client.price(stock.symbol))
-  rescue => exception 
-  end
-end
+# selected_symbols.each do|stock| 
+#   begin
+#    Stock.find_or_create_by('ticker'=>stock.symbol, 'name' => client.company(stock.symbol).company_name,'last_price'=>client.price(stock.symbol))
+#   rescue => exception 
+#   end
+# end
 
-User.create!([
-  {username:'stockjobberadmin', email: "admin@stockjobber.com", password: "password", password_confirmation: "password", role: 'admin', approved: true},
-  {username:'eileenadmin', email: "eileen_admin@stockjobber.com", password: "password", password_confirmation: "password", role: 'admin', approved: true},
-  {username:'ralphadmin', email: "ralph_admin@stockjobber.com", password: "password", password_confirmation: "password", role: 'admin', approved: true}
-])
+# User.create!([
+#   {username:'stockjobberadmin', email: "admin@stockjobber.com", password: "password", password_confirmation: "password", role: 'admin', approved: true},
+#   {username:'eileenadmin', email: "eileen_admin@stockjobber.com", password: "password", password_confirmation: "password", role: 'admin', approved: true},
+#   {username:'ralphadmin', email: "ralph_admin@stockjobber.com", password: "password", password_confirmation: "password", role: 'admin', approved: true}
+# ])
