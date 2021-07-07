589897d001e1bc766bc89f08d046f016eddb38aa
Eunicegenel <eunel_soliveres@yahoo.com>
2021-07-07 14:32:52 +0800

    changed comment column in comments to body

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_055300) do
+ActiveRecord::Schema.define(version: 2021_07_07_063145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 2021_07_07_055300) do
   create_table "comments", force: :cascade do |t|
     t.integer "commenter_id"
     t.integer "item_id"
-    t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "body"
   end
 
   create_table "items", force: :cascade do |t|
