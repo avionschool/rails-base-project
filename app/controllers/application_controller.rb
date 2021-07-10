4380113d836f0769317d434e9d4e4f7f173076cd
Leo <leocsdev@gmail.com>
2021-07-10 11:40:56 +0800

    Run rubocop -A

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(
-      :sign_up, keys: [
-        :first_name, 
-        :last_name, 
-        :address, 
-        :contact
-      ]
-    )
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name address contact])
   end
 end
