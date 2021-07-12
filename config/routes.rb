2185221057aa90f7671c61ed20a0669943f952af
EliezerAmbion <eliezerambion@gmail.com>
2021-07-12 20:49:28 +0800

    generated devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+
+  # root "home#index"
 end
