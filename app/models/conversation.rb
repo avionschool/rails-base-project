9ae20a1adc9ce186c89bbc1f158c1855e549cf5c
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-13 21:07:49 +0800

    added associations and class methods

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,3 +1,11 @@
 class Conversation < ApplicationRecord
-    has_many :messages
+    # how to approach dependent of messages? if a convo is destroyed, the other user's copy should not be automatically deleted
+    has_many :messages, class_name: "Message", foreign_key: "conversation_id"
+    belongs_to :user1, class_name: "User"
+    belongs_to :user2, class_name: "User"
+
+    # returns conversation participants in array
+    def participants
+        [self.user1_id, self.user2_id]
+    end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ApplicationRecord
-    belongs_to :user
-    belongs_to :conversation
+    belongs_to :conversation, class_name: "Conversation"
+    belongs_to :user, class_name: "User"
+
+    def sender
+        self.user_id
+    end
 end
