841f5f7377c771e1b8d5545f9efc6fe3a9a98d4f
Eunicegenel <eunel_soliveres@yahoo.com>
2021-07-13 19:40:03 +0800

    validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, :contact, :address, presence: true
+
+  has_many :items
+  has_many :comments
+  has_many :reviews
 end
