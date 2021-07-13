da12165f85aa206ee647a5d18a8c303f32deb7d4
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-14 01:36:03 +0800

    added poster's email in the item listing

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :status, presence: true
 
-  belongs_to :user
+  belongs_to :user, class_name: 'User'
   has_many :comments, dependent: :restrict_with_exception
 end
