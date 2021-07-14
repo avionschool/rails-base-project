f54100b8cbc64e37dbcdd87f166f3573404af2e2
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-14 21:38:12 +0800

    added associations for items and conversations

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,7 @@ class Item < ApplicationRecord
   validates :status, presence: true
 
   belongs_to :user, class_name: 'User'
+  has_many :conversations, dependent: :destroy
   has_many :comments, dependent: :restrict_with_exception
+  
 end
