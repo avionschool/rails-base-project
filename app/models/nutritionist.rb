322c41ae79dca200809e94878387aed7f1e9e447
desoto13 <mrmisa_windel@yahoo.com>
2021-07-14 09:50:43 +0800

    generated a new master key and credentials

--- app/models/nutritionist.rb
+++ app/models/nutritionist.rb
@@ -17,10 +17,4 @@ class Nutritionist < ApplicationRecord
   def inactive_message
     approved? ? super : :not_approved
   end
-
-  # after_create :send_admin_mail
-  # def send_admin_mail
-  #   BrokerMailer.new_broker_account_pending(email).deliver
-  #   AdminMailer.new_user_waiting_for_approval(email).deliver
-  # end
 end
