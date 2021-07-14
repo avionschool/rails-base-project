e732eb538dff733ff913447ca772e01e0863bcce
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-14 22:20:41 +0800

    resolved requested changes

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :status, presence: true
 
-  belongs_to :user, class_name: 'User'
+  belongs_to :user
   has_many :conversations, dependent: :destroy
   has_many :comments, dependent: :restrict_with_exception
 end
