9b87ff94265e0752fb54e56b461a3ac9a8c6a401
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-14 19:24:18 +0800

    added conversation detection

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -8,17 +8,25 @@ class ConversationsController < ApplicationController
     end
 
     def create
-        @conversation = Conversation.new(conversation_params)
-    
-        respond_to do |format|
-          if @conversation.save
-            format.html { redirect_to @conversation, notice: "Conversation was successfully created." }
-            format.json { render :show, status: :created, location: @conversation }
-          else
-            format.html { render :new, status: :unprocessable_entity }
-            format.json { render json: @conversation.errors, status: :unprocessable_entity }
-          end
+        if Conversation.exists?(conversation_params)
+            redirect_to request.referrer, :alert => 'Conversation exists!'
+        else
+            @conversation = Conversation.new(conversation_params)
+            @conversation.save
+            redirect_to request.referrer, :alert => 'New conversation created!'
         end
+        
+
+    
+        # respond_to do |format|
+        #   if @conversation.save
+        #     format.html { redirect_to @conversation, notice: "Conversation was successfully created." }
+        #     format.json { render :show, status: :created, location: @conversation }
+        #   else
+        #     format.html { render :new, status: :unprocessable_entity }
+        #     format.json { render json: @conversation.errors, status: :unprocessable_entity }
+        #   end
+        # end
       end
 
       private
