aafa0943f6d57b1ac24f8558c5ace368ba41e5b0
Leo <leocsdev@gmail.com>
2021-07-14 11:06:18 +0800

    Added letter opener config in prod.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.default_url_options = { host: 'https://kalakalph.herokuapp.com/'}
+
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
