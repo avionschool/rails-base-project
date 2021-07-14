86be0c3665a1211f20993bd143d36fd395fd3048
Leo <leocsdev@gmail.com>
2021-07-14 09:13:11 +0800

    Mount letter_opener in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  
   root to: 'home#index'
   
   get '/items/:id' => 'items#index', as: 'items_index'
