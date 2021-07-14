9961c9cef519f26017f7ab83a7c4f97a49ac7855
Leo <leocsdev@gmail.com>
2021-07-14 10:16:30 +0800

    Added confirmations controller in devise users, redirects to whatever path set after user confirmed their email.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   
+  devise_for :users, controllers: {
+    confirmations: 'confirmations'
+  }
+  
   root to: 'home#index'
   
   get '/items/:id' => 'items#index', as: 'items_index'
   post '/items/create/:id' => 'items#create', as: 'items_create'
-  devise_for :users
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
