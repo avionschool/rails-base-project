750ebb02fc9893d7a287617fa7110372e1c5aa54
AdrianIlaga <adrianilaga771@gmail.com>
2021-07-16 00:48:49 +0800

    added cpu scraper

--- app/models/cpu.rb
+++ app/models/cpu.rb
@@ -1,2 +1,21 @@
 class Cpu < ApplicationRecord
+  def self.name
+    @name = 'cpus'
+  end
+
+  def self.url
+    @start_urls = ["https://pangoly.com/en/browse/cpu?page=#{@page}"]
+  end
+
+  def self.parse_repo_page(response)
+    puts('getting info')
+    item = {}
+    item[:socket] = response.xpath("//table[@class='table table-bordered table-striped table-hover']/tbody").css('tr')[4].css('td')[1].text.squish
+    item[:core_count] = response.xpath("//table[@class='table table-bordered table-striped table-hover']/tbody").css('tr')[6].css('td')[1].text.squish.to_i
+    item[:core_clock] = response.xpath("//table[@class='table table-bordered table-striped table-hover']/tbody").css('tr')[8].css('td')[1].text.squish.to_i
+    item[:boost_clock] = response.xpath("//table[@class='table table-bordered table-striped table-hover']/tbody").css('tr')[9].css('td')[1].text.squish.to_i
+    item[:tdp] = response.xpath("//table[@class='table table-bordered table-striped table-hover']/tbody").css('tr')[10].css('td')[1].text.squish
+    item[:price] = response.xpath("//span[@class='price']").text.tr('^0-9.', '').to_f
+    self.find_or_create_by(item)
+  end
 end
---
+++ app/models/scraper.rb
@@ -0,0 +1,34 @@
+require 'kimurai'
+require 'webdrivers'
+
+class Scraper < Kimurai::Base
+  @name = 'scraper'
+  @start_urls = []
+  @page = 1
+  @engine = :selenium_firefox
+  @config = { before_request: {
+    delay: 4..6
+  } }
+
+  def self.process(model)
+    @@class = model
+    @name = @@class.name
+    @start_urls = @@class.url
+    self.crawl!
+  end
+
+  def parse(response, url:, data: {})
+    puts 'getting response'
+    page = browser.current_response  
+    page.xpath("//a[contains(@class, 'productItemLink')]").each do |a|
+      puts 'getting link'
+      request_to :parse_repo_page, url: absolute_url(a[:href], base: url)
+      puts 'next item'
+    end
+    @page += 1
+  end
+
+  def parse_repo_page(response, url:, data: {})
+    @@class.parse_repo_page(response)
+  end
+end 
