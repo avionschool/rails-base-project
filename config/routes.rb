cd16cc1999c1a27bc4acaf3344b59126c59ea8c4
Leif Bersamina <leifbersamina@gmail.com>
2021-07-16 22:55:16 +0800

    add resources and devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :tourists
+  devise_for :agencies
+  devise_for :admins, skip: :registration
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
+
+  resources :admins
+  resources :agencies
 end
