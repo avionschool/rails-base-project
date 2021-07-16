b8a6b83b6238cc4f995747e55049bc250540fa42
AdrianIlaga <72000424+AdrianIlaga@users.noreply.github.com>
2021-07-16 18:49:44 +0800

    added incomplete scraper for cpu

--- app/models/cpu.rb
+++ app/models/cpu.rb
@@ -12,12 +12,32 @@ class Cpu < ApplicationRecord
   def self.parse_repo_page(response)
     puts('getting info')
     item = {}
-    item[:socket] = response.xpath("//table[@class='table table-bordered table-striped table-hover']/tbody").css('tr').xpath("//strong[text()='Socket']").xpath('..').xpath('..').css('td')[1].text.squish
-    item[:core_count] = response.xpath("//table[@class='table table-bordered table-striped table-hover']/tbody").css('tr').xpath("//strong[text()='CPU Cores']").xpath('..').xpath('..').css('td')[1].text.squish.to_i
-    item[:core_clock] = response.xpath("//table[@class='table table-bordered table-striped table-hover']/tbody").css('tr').xpath("//strong[text()='CPU Clock Rate']").xpath('..').xpath('..').css('td')[1].text.squish.to_i
-    item[:boost_clock] = response.xpath("//table[@class='table table-bordered table-striped table-hover']/tbody").css('tr').xpath("//strong[text()='CPU Turbo Clock Rate']").xpath('..').xpath('..').css('td')[1].text.squish.to_i
-    item[:tdp] = response.xpath("//table[@class='table table-bordered table-striped table-hover']/tbody").css('tr').xpath("//strong[text()='TDP']").xpath('..').xpath('..').css('td')[1].text.squish
+    @table = response.xpath("//table[@class='table table-bordered table-striped table-hover']/tbody")
+    item[:socket] = self.get_text('Socket')
+    item[:core_count] = self.get_int('CPU Cores')
+    item[:core_clock] = self.get_int('CPU Clock Rate')
+    item[:boost_clock] = self.get_int('CPU Turbo Clock Rate')
+    item[:tdp] = self.get_text('TDP')
     item[:price] = response.xpath("//span[@class='price']").text.tr('^0-9.', '').to_f
+    byebug
     self.find_or_create_by(item)
+  end
+
+  def self.get_text(column)
+    info = @table.css('tr').xpath("//strong[text()='#{column}']").xpath('..').xpath('..').css('td')[1]
+    return nil unless info
+    return info.text.squish
+  end
+
+  def self.get_int(column)
+    info = @table.css('tr').xpath("//strong[text()='#{column}']").xpath('..').xpath('..').css('td')[1]
+    return nil unless info
+    return info.text.squish.to_i
+  end
+  
+  def self.get_arr(column)
+    info = @table.css('tr').xpath("//strong[text()='#{column}']").xpath('..').xpath('..').css('td')[1]
+    return nil unless info
+    return info.css('ul/li').map{ |item| item.text }
   end
 end
