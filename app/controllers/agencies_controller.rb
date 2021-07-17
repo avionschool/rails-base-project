9efe86180b935ab7f8c61f7d5cb0623f5a4b6ead
Leif Bersamina <leifbersamina@gmail.com>
2021-07-18 01:01:39 +0800

    add agency update method

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -11,4 +11,20 @@ class AgenciesController < ApplicationController
     @agency = Agency.find(params[:id])
     @tours = @agency.tours
   end
+
+  def update
+    @agency = Agency.find(params[:id])
+    if @agency.valid? && @agency.update(agency_params)
+      @agency.save
+      redirect_to admins_path, notice: 'Agency approved!'
+    else
+      redirect_back fallback_location: agencies_path, alert: @agency.errors.first
+    end
+  end
+
+  private
+
+  def agency_params
+    params.require(:agency).permit(:verified_by, :approved)
+  end
 end
