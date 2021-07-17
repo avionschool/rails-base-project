309d01e37aaafb0e98c84fbec35a30d102e64e2f
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-07-17 21:24:44 +0800

    modify routes add tourist_tour routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :tourists
   devise_for :agencies
   devise_for :admins, skip: :registration
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root "home#index"
 
   resources :admins
@@ -11,4 +13,6 @@ Rails.application.routes.draw do
   resources :tours, except: [:destroy]
   resources :tourist_tours, except: [:new, :edit, :update]
   resources :travel_transactions, only: [:index]
+  get '/tourist_tours/new/:tour_id', to: 'tourist_tours#new', as: 'new_tourist_tour'
+  
 end
