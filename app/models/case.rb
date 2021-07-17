74503ae3c3342d05cfdafd0c7a8ce023679855b8
AdrianIlaga <72000424+AdrianIlaga@users.noreply.github.com>
2021-07-17 10:27:04 +0800

    added completed scraper for case model

--- app/models/case.rb
+++ app/models/case.rb
@@ -13,17 +13,22 @@ class Case < ApplicationRecord
     puts('getting info')
     item = {}
     @table = response.xpath("//table[@class='table table-bordered table-striped table-hover']/tbody")
-    item[:type] = self.get_text('Type')
+    item[:form_factor] = self.get_text('Type')
     item[:color] = self.get_text('Color')
-    item[:power_supply] = @table.css('tr').xpath("//strong[text()='Includes Power Supply']").xpath('..').xpath('..').css('td')[1].css('span').first.to_a[0][1]
+    item[:power_supply] = @table.css('tr').xpath("//strong[text()='Includes Power Supply']").xpath('..').xpath('..').css('td')[1].css('span').attr('title').value
     if item[:power_supply] == "Yes"
       item[:power_supply] = true
-    else
+    elsif item[:power_supply] == "No"
       item[:power_supply] = false
+    else
+      item[:power_supply] = nil
     end
     item[:bay_2_5] = self.get_int('Internal 2.5" Drive Bays')
     item[:bay_3_5] = self.get_int('Internal 3.5" Drive Bays')
     item[:price] = response.xpath("//span[@class='price']").text.tr('^0-9.', '').to_f
+    item[:name] = response.xpath("//div[contains(@class, 'product-info')]").css('h2').text
+    item[:image] = response.xpath("//img[@class='nivo-main-image']").attr('src').value
+    item[:motherboard_form_factor] = self.get_text('Motherboard Compatibility').split(',')
     byebug
     self.find_or_create_by(item)
   end
