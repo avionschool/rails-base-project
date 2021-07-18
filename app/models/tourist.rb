2be09c545e2291cda4c9f3110e49e7eee79264c7
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-07-18 16:22:48 +0800

    edit tourist, agency and chat room model to fix association issue

--- app/models/tourist.rb
+++ app/models/tourist.rb
@@ -3,8 +3,9 @@ class Tourist < User
   has_many :tours, through: :tourist_tours
 
   has_many :travel_transactions, through: :tourist_tours
-
   has_many :reviews, dependent: :nullify
+  has_many :chat_room_users, dependent: :nullify, foreign_key: 'user_id', inverse_of: :user
+  has_many :chat_rooms, through: :chat_room_users
 
   after_create :set_approved_to_true
 
