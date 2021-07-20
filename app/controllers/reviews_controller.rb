0cc2dee76539b191ce1caee9b9ccc81f0d99b1ba
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-21 01:38:19 +0800

    fixed bug for users getting more than 1 review from a single transaction

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,16 +7,26 @@ class ReviewsController < ApplicationController
     @item = Item.find(params[:item_id])
     @user1 = @item.user                             # user1 is the item owner ('seller')
     @transact = Transact.find(params[:transact_id])
-    @user2 = User.find(@transact.user2_id)           # user2 is the one who availed the item ('buyer')
+    @user2 = User.find(@transact.user2_id) # user2 is the one who availed the item ('buyer')
   end
 
   def create
     @review = Review.new(review_params)
-    if @review.save
-      redirect_to root_path, notice: 'Review was submitted succesfully.'
-    else
-      redirect_to request.referer, alert: 'Review failed to submit.'
+
+    # check first if review already exists
+    if params[:user1_id] == nil                 # user1 is giving the review
+        review_check = Review.find_by(transact_id: params[:transact_id], user2_id: params[:review][:user2_id])
+        review_check.delete if review_check        
+    elsif params[:user2_id] == nil              # user2 is giving the review
+        review_check = Review.find_by(transact_id: params[:transact_id], user1_id: params[:review][:user1_id])
+        review_check.delete if review_check
     end
+    if @review.save
+        redirect_to root_path, notice: 'Review was submitted succesfully.' 
+    else
+        redirect_to request.referer, alert: 'Review failed to submit.'
+    end
+    
   end
 
   private
