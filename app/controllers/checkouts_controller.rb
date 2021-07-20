e309ca0adcdbdbb54d2cf21e2fcd824ed968d46d
Leif Bersamina <leifbersamina@gmail.com>
2021-07-20 19:20:48 +0800

    fixed rubocop errors

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,34 +1,33 @@
 class CheckoutsController < ApplicationController
-
   def create
     Stripe.api_key = Rails.application.credentials.stripe[:secret_key]
-        @tour = Tour.find(params[:tour_id])
-        @session = Stripe::Checkout::Session.create({
-        payment_method_types: ['card'],
-        line_items: [{ 
-            name: @tour.name, 
-            amount: (@tour.price * 100).to_i, 
-            currency: 'usd', 
-            quantity: params[:quantity] 
-            }], 
-        payment_intent_data: { 
-          metadata: { 
-            tourist_id: current_tourist.id,
-            tour_id: @tour.id, 
-            price: @tour.price,
-            start_date: params[:start_date],
-            end_date: params[:end_date],
-            guest_quantity: params[:quantity],
-            total_amount: params[:amount]
-          } 
-        }, 
-        mode: 'payment',
-        success_url: root_url,
-        cancel_url: new_tourist_tour_url(@tour)
-        })
+    @tour = Tour.find(params[:tour_id])
+    @session = Stripe::Checkout::Session.create({
+                                                  payment_method_types: ['card'],
+    line_items: [{
+      name: @tour.name,
+        amount: (@tour.price * 100).to_i,
+        currency: 'usd',
+        quantity: params[:quantity]
+    }],
+    payment_intent_data: {
+      metadata: {
+        tourist_id: current_tourist.id,
+        tour_id: @tour.id,
+        price: @tour.price,
+        start_date: params[:start_date],
+        end_date: params[:end_date],
+        guest_quantity: params[:quantity],
+        total_amount: params[:amount]
+      }
+    },
+    mode: 'payment',
+    success_url: root_url,
+    cancel_url: new_tourist_tour_url(@tour)
+                                                })
 
-        respond_to do |format|
-            format.js
-        end
+    respond_to do |format|
+      format.js
+    end
   end
 end
--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,47 +1,51 @@
 class WebhooksController < ApplicationController
-  skip_before_action :verify_authenticity_token 
+  skip_before_action :verify_authenticity_token
 
   def create
-      payload = request.body.read
-      sig_header = request.env['HTTP_STRIPE_SIGNATURE']
-      event = nil
+    payload = request.body.read
+    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
+    event = nil
 
-      begin
+    begin
       event = Stripe::Webhook.construct_event(
-          payload, sig_header, Rails.application.credentials[:stripe][:webhook]
+        payload, sig_header, Rails.application.credentials[:stripe][:webhook]
       )
-      rescue JSON::ParserError => e
+    rescue JSON::ParserError => e
+      logger.error(e)
       status 400
       return
-      rescue Stripe::SignatureVerificationError => e
+    rescue Stripe::SignatureVerificationError => e
       # Invalid signature
-      puts "Signature error"
-      p e
+      logger = Logger.new($stdout)
+      logger.error('Signature error')
+      logger.error(e)
       return
-      end
+    end
 
-      # Handle the event
-      case event.type
-      when 'checkout.session.completed'
+    # Handle the event
+    case event.type
+    when 'checkout.session.completed'
+      logger = Logger.new($stdout)
       session = event.data.object
       payment_intent = session.payment_intent
       stripe = Stripe::PaymentIntent.retrieve(payment_intent)
       data = stripe.metadata
-      puts "THIS IS THE data #{data}"
-      puts current_tourist
-      @tourist_tour = TouristTour.create(
-      tour_id: data.tour_id, 
-      tourist_id: data.tourist_id, 
-      amount_bought: data.total_amount, 
-      guest_quantity: data.guest_quantity,
-      start_date: data.start_date,
-      end_date: data.end_date )
-      tour = Tour.find(data.tour_id)
-      @travel_transaction = TravelTransaction.create(tourist_tour: @tourist_tour, 
-          agency_id: tour.agency.id, 
-          total_price: data.total_amount)
-      end
 
-      render json: { message: 'success' }
+      logger.error("This is the data #{data}")
+      @tourist_tour = TouristTour.create(
+        tour_id: data.tour_id,
+        tourist_id: data.tourist_id,
+        amount_bought: data.total_amount,
+        guest_quantity: data.guest_quantity,
+        start_date: data.start_date,
+        end_date: data.end_date
+      )
+      tour = Tour.find(data.tour_id)
+      @travel_transaction = TravelTransaction.create(tourist_tour: @tourist_tour,
+          agency_id: tour.agency.id,
+          total_price: data.total_amount)
+    end
+
+    render json: { message: 'success' }
   end
 end
