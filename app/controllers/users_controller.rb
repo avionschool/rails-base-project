17dc4712c5ef085d80a6758036d9adfdec88695d
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-20 08:08:18 +0800

    improved user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+    @user = User.find(params[:id])
+    @conversations = current_user.conversations
+  end
+
+  def history
     @user = User.find(params[:id])
   end
 
