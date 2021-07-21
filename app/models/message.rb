d250d98ae8fb57438c8dbc6180bbcfd0a37805c5
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-22 07:07:37 +0800

    disabled sending message when sending a white space; added links to commenter profile page in comment section

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,8 @@ class Message < ApplicationRecord
   belongs_to :conversation, class_name: 'Conversation'
   belongs_to :user, class_name: 'User'
 
+  validates :content, presence: true
+
   def sender
     user_id
   end
