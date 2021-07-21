4782e45f6fcff83b3630280197d66e6a102b1ae1
Eunicegenel <eunel_soliveres@yahoo.com>
2021-07-21 09:23:09 +0800

    rebased schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_120712) do
+ActiveRecord::Schema.define(version: 2021_07_20_163937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,18 @@ ActiveRecord::Schema.define(version: 2021_07_20_120712) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "transact_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "street"
+    t.string "suburb"
+    t.string "city"
+    t.string "landmark"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "messages", force: :cascade do |t|
