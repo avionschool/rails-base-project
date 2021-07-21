efb2f55647f416fe71fe2758a44874d6cea80279
Eunicegenel <eunel_soliveres@yahoo.com>
2021-07-21 09:39:25 +0800

    rubocop fixes

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,58 +1,58 @@
 class LocationsController < ApplicationController
-  def index 
+  def index
     @location = current_user.locations.last
   end
 
   def create
     place = current_user.locations.new(location_params)
-    if place.save 
-      place1 = [place.street, place.suburb, place.city].compact.join(', ')
-      place2 = [place.landmark, place.city].compact.join(', ')
-      place3 = [place.suburb, place.city].compact.join(', ')
-      results1 = Geocoder.search(place1)
-      results2 = Geocoder.search(place2)
-      results3 = Geocoder.search(place3)
-      results4 = Geocoder.search(place.city)
+    return unless place.save
 
-      if results1.present?
-        coords = results1.first.coordinates
-      elsif results2.present?
-        coords = results2.first.coordinates
-      elsif results2.present?
-        coords = results2.first.coordinates
-      else
-        coords = results4.first.coordinates
-      end
+    place1 = [place.street, place.suburb, place.city].compact.join(', ')
+    place2 = [place.landmark, place.city].compact.join(', ')
+    place3 = [place.suburb, place.city].compact.join(', ')
+    results1 = Geocoder.search(place1)
+    results2 = Geocoder.search(place2)
+    results3 = Geocoder.search(place3)
+    results4 = Geocoder.search(place.city)
 
-      place.update(latitude: coords[0], longitude: coords[1])
-      redirect_to edit_user_registration_path
-    end
+    coords = if results1.present?
+               results1.first.coordinates
+             elsif results2.present?
+               results2.first.coordinates
+             elsif results3.present?
+               results3.first.coordinates
+             else
+               results4.first.coordinates
+             end
+
+    place.update(latitude: coords[0], longitude: coords[1])
+    redirect_to edit_user_registration_path
   end
 
   def update
     place = current_user.locations.new(location_params)
-    if current_user.locations.first.update(location_params)
-      place1 = [place.street, place.suburb, place.city].compact.join(', ')
-      place2 = [place.landmark, place.city].compact.join(', ')
-      place3 = [place.suburb, place.city].compact.join(', ')
-      results1 = Geocoder.search(place1)
-      results2 = Geocoder.search(place2)
-      results3 = Geocoder.search(place3)
-      results4 = Geocoder.search(place.city)
+    return unless current_user.locations.first.update(location_params)
 
-      if results1.present?
-        coords = results1.first.coordinates
-      elsif results2.present?
-        coords = results2.first.coordinates
-      elsif results2.present?
-        coords = results2.first.coordinates
-      else
-        coords = results4.first.coordinates
-      end
+    place1 = [place.street, place.suburb, place.city].compact.join(', ')
+    place2 = [place.landmark, place.city].compact.join(', ')
+    place3 = [place.suburb, place.city].compact.join(', ')
+    results1 = Geocoder.search(place1)
+    results2 = Geocoder.search(place2)
+    results3 = Geocoder.search(place3)
+    results4 = Geocoder.search(place.city)
 
-      current_user.locations.first.update(latitude: coords[0], longitude: coords[1])
-      redirect_to edit_user_registration_path
-    end
+    coords = if results1.present?
+               results1.first.coordinates
+             elsif results2.present?
+               results2.first.coordinates
+             elsif results3.present?
+               results3.first.coordinates
+             else
+               results4.first.coordinates
+             end
+
+    current_user.locations.first.update(latitude: coords[0], longitude: coords[1])
+    redirect_to edit_user_registration_path
   end
 
   private
