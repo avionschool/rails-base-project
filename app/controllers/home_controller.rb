1da8cefb9adac274612ec62592967a863ccf2186
Eunicegenel <eunel_soliveres@yahoo.com>
2021-07-23 02:40:31 +0800

    rubocop fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,11 @@ class HomeController < ApplicationController
   def index
     @conversations = current_user.conversations if user_signed_in?
     if user_signed_in?
-      items = Item.where("user_id not in(?)",current_user.id)
+      items = Item.where('user_id not in(?)', current_user.id)
       @items = items.where(status: 'open').sort_by(&:created_at).reverse if current_user.locations.first.nil?
 
       return if current_user.locations.first.nil?
+
       @items = current_user.items_from_user[0] + current_user.items_from_user[1]
     else
       @items = Item.where(status: 'open').sort_by(&:created_at).reverse
