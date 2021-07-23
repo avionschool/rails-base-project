92c19cb8e376cda15d8e0c7740b4a6fd2cd0da3d
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-07-23 23:44:03 +0800

    all travel transactions pagination

--- app/controllers/travel_transactions_controller.rb
+++ app/controllers/travel_transactions_controller.rb
@@ -3,9 +3,9 @@ class TravelTransactionsController < ApplicationController
 
   def index
     if tourist_signed_in? || agency_signed_in?
-      @travel_transactions = current_user.travel_transactions
+      @travel_transactions = current_user.travel_transactions.paginate(page: params[:page], per_page: 20)
     elsif admin_signed_in?
-      @travel_transactions = TravelTransaction.all
+      @travel_transactions = TravelTransaction.all.paginate(page: params[:page], per_page: 20)
     end
   end
 end
