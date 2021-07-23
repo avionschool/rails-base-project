5f13110f9ea9d7ef032ab3b99897c6392bac5c07
JC <proxybetazero@gmail.com>
2021-07-23 19:24:10 +0800

    fixed rspec errors

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   # like functionality
   acts_as_votable
 
-  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   belongs_to :tourist
   belongs_to :agency
