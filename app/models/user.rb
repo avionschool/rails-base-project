54c53032ba59fda20aeb160ec3d21927d034f819
MabuPGithub <mylespison@yahoo.com>
2021-07-27 13:24:09 +0800

    added locales and users scss, edited user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def active_for_authentication? 
+    super && approved? 
+  end 
+
+  def inactive_message 
+    approved? ? super : :not_approved
+  end
+
+  after_create :send_admin_mail
+  def send_admin_mail
+    AdminMailer.new_user_waiting_for_approval(email).deliver
+  end
 end
