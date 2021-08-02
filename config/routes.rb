a9314218161387277e8470cfd248a18f7aa974a6
MabuPGithub <mylespison@yahoo.com>
2021-08-02 20:55:11 +0800

    fixed user controllers with confirmable

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
 
-  root to: 'users#index', as: 'users_index'
-  devise_for :users, :controllers => { registrations: 'users/registrations' }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => { registrations: 'users/registrations', sessions: 'users/sessions' }
+  root to: 'stocks_view#index'
   resources :users
   get '/get/stock-view' => 'stocks_view#index', as: 'stock_view_index'
   get '/get/stock-view/:stocksymbol' => 'stocks_view#show', as: 'stock_view_show' 
-  # get '/categories' => 'categories#index', as: 'categories_index'
 end
