ee02d4696937b663d552ba28489c6e5bab1bfb52
Brando <brandomoreno06@outlook.com>
2021-09-08 20:08:26 +0800

    updated .tool-versions, modified users modela and schema

--- db/migrate/20210907131358_add_details_to_users.rb
+++ db/migrate/20210907131358_add_details_to_users.rb
@@ -3,7 +3,7 @@ class AddDetailsToUsers < ActiveRecord::Migration[6.0]
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :username, :string
-    add_column :users, :role, :integer
-    add_column :users, :status, :integer
+    add_column :users, :role, :integer, default: 0
+    add_column :users, :status, :integer, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(version: 2021_09_07_131358) do
     t.string "first_name"
     t.string "last_name"
     t.string "username"
-    t.integer "role"
-    t.integer "status"
+    t.integer "role", default: 0
+    t.integer "status", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
