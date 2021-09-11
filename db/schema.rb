acb7f1833a834cd2393fa519c6896820a512cf4b
chestergarett <chestergarett@gmail.com>
2021-09-11 10:59:00 +0800

    reverting changes in schema due to bug found

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_11_022439) do
+ActiveRecord::Schema.define(version: 2021_09_08_131047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,5 +28,4 @@ ActiveRecord::Schema.define(version: 2021_09_11_022439) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
-
 end
