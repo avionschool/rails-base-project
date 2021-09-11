665be1fc22b66ce5970f421468974be3b27e86fb
Warda Nur <wardanur04@gmail.com>
2021-09-11 18:00:51 +0800

    fixed rspec error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def configure_permitted_parameters;
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
