c5228a35617362cbf5dfa0ef3c67501bbed9767a
chestergarett <chestergarett@gmail.com>
2021-09-11 10:57:20 +0800

    reverting changes in schema due to bug

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,6 @@ ActiveRecord::Schema.define(version: 2021_09_11_022439) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "roles", force: :cascade do |t|
-    t.string "role"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
