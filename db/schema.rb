061f0698f45046d1a2550944f5897d0615a120cb
chestergarett <chestergarett@gmail.com>
2021-09-11 10:41:49 +0800

    fixing bug, unable to sign up because of lacking clause in validate_username

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_08_131047) do
+ActiveRecord::Schema.define(version: 2021_09_11_022439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "roles", force: :cascade do |t|
+    t.string "role"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_09_08_131047) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
+
 end
