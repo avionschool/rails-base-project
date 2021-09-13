8503dd7cd1927c4fc21691d8e8adc1891b25b3c0
chestergarett <chestergarett@gmail.com>
2021-09-13 21:57:37 +0800

    resolving PR comments on UI page

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,11 +4,11 @@ const webpack = require('webpack');
 
 environment.plugins.append(
   'Provide',
-	new webpack.ProvidePlugin({
-		$: 'jquery',
-		jQuery: 'jquery',
-		Popper: ['popper.js', 'default'],
-	})
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default'],
+  })
 );
 
 module.exports = environment
