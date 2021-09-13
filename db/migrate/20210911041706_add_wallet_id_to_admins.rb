d98659f16d964f507ef5a4d6ee3ae67c374731ef
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-13 21:47:59 +0800

    changed format of views and fixed line and indententions, removed wallet_id in admins table

--- db/migrate/20210911041706_add_wallet_id_to_admins.rb
+++
@@ -1,5 +0,0 @@
-class AddWalletIdToAdmins < ActiveRecord::Migration[6.0]
-  def change
-    add_column :admins, :wallet_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_11_041706) do
+ActiveRecord::Schema.define(version: 2021_09_11_041656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 2021_09_11_041706) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "full_name"
     t.string "username"
-    t.integer "wallet_id"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
