9f485e975e90432da5d8815cd53c0f13e88bdd3c
Steven <stevenchoy0915@gmail.com>
2021-09-13 21:20:42 +0800

    updated rubocop correctables

--- spec/model/trial_user_spec.rb
+++ spec/model/trial_user_spec.rb
@@ -8,22 +8,21 @@ RSpec.describe User, type: :model do
 
   it 'is not valid without first_name' do
     user2 = described_class.new(first_name: nil)
-    expect(user2).to_not be_valid
+    expect(user2).not_to be_valid
   end
 
   it 'is not valid without last_name' do
     user2 = described_class.new(last_name: nil)
-    expect(user2).to_not be_valid
+    expect(user2).not_to be_valid
   end
 
   it 'is not valid without email' do
     user2 = described_class.new(email: nil)
-    expect(user2).to_not be_valid
+    expect(user2).not_to be_valid
   end
 
   it 'is not valid without username' do
     user2 = described_class.new(username: nil)
-    expect(user2).to_not be_valid
+    expect(user2).not_to be_valid
   end
-
 end
