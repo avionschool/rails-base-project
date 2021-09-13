1b1a41d1cc1c247d41778f98cff84a66dffb9144
Steven <stevenchoy0915@gmail.com>
2021-09-13 20:34:37 +0800

    made changes to the quotation and changed User into described.class

--- spec/models/trial_user_spec.rb
+++ spec/models/trial_user_spec.rb
@@ -1,28 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is valid with valid attributes" do
-    user1 = User.new(email: 'email@example.com', password: 'password')
+  it 'is valid with valid attributes' do
+    user1 = described_class.new(email: 'email@example.com', password: 'password')
     expect(user1).to be_valid
   end
 
-  it "is not valid without first_name" do
-    user2 = User.new(first_name: nil)
+  it 'is not valid without first_name' do
+    user2 = described_class.new(first_name: nil)
     expect(user2).to_not be_valid
   end
 
-  it "is not valid without last_name" do
-    user2 = User.new(last_name: nil)
+  it 'is not valid without last_name' do
+    user2 = described_class.new(last_name: nil)
     expect(user2).to_not be_valid
   end
 
-  it "is not valid without email" do
-    user2 = User.new(email: nil)
+  it 'is not valid without email' do
+    user2 = described_class.new(email: nil)
     expect(user2).to_not be_valid
   end
 
-  it "is not valid without username" do
-    user2 = User.new(username: nil)
+  it 'is not valid without username' do
+    user2 = described_class.new(username: nil)
     expect(user2).to_not be_valid
   end
 
