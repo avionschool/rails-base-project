8ff0b05295bb747aa1c2ebc81126b9a94ee85702
Steven <stevenchoy0915@gmail.com>
2021-09-14 20:25:45 +0800

    rubocop configures and addressed the offenses

--- spec/models/trial_user_spec.rb
+++ spec/models/trial_user_spec.rb
@@ -1,28 +1,26 @@
 require 'rails_helper'
 
-  it "is valid with valid attributes" do
-    user1 = User.new(email: 'email@example.com', password: 'password')
-    expect(user1).to be_valid
-  end
+it 'is valid with valid attributes' do
+  user1 = User.new(email: 'email@example.com', password: 'password')
+  expect(user1).to be_valid
+end
 
-  it "is not valid without first_name" do
-    user2 = User.new(first_name: nil)
-    expect(user2).to_not be_valid
-  end
+it 'is not valid without first_name' do
+  user2 = User.new(first_name: nil)
+  expect(user2).not_to be_valid
+end
 
-  it "is not valid without last_name" do
-    user2 = User.new(last_name: nil)
-    expect(user2).to_not be_valid
-  end
+it 'is not valid without last_name' do
+  user2 = User.new(last_name: nil)
+  expect(user2).not_to be_valid
+end
 
-  it "is not valid without email" do
-    user2 = User.new(email: nil)
-    expect(user2).to_not be_valid
-  end
+it 'is not valid without email' do
+  user2 = User.new(email: nil)
+  expect(user2).not_to be_valid
+end
 
-  it "is not valid without username" do
-    user2 = User.new(username: nil)
-    expect(user2).to_not be_valid
-  end
-
+it 'is not valid without username' do
+  user2 = User.new(username: nil)
+  expect(user2).not_to be_valid
 end
