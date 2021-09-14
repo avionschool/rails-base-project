a63702501153421bc120ac9a327fa913ed2f9295
Steven <stevenchoy0915@gmail.com>
2021-09-14 20:51:42 +0800

    rubocop -A on the rspec

--- spec/models/trial_user_spec.rb
+++ spec/models/trial_user_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
   it 'is valid with valid attributes' do
-    user1 = User.new(email: 'email@example.com', password: 'password')
+    user1 = described_class.new(email: 'email@example.com', password: 'password')
     expect(user1).to be_valid
   end
 
   it 'is not valid without first_name' do
-    user2 = User.new(first_name: nil)
+    user2 = described_class.new(first_name: nil)
     expect(user2).not_to be_valid
   end
 
   it 'is not valid without last_name' do
-    user2 = User.new(last_name: nil)
+    user2 = described_class.new(last_name: nil)
     expect(user2).not_to be_valid
   end
 
   it 'is not valid without email' do
-    user2 = User.new(email: nil)
+    user2 = described_class.new(email: nil)
     expect(user2).not_to be_valid
   end
 
   it 'is not valid without username' do
-    user2 = User.new(username: nil)
+    user2 = described_class.new(username: nil)
     expect(user2).not_to be_valid
   end
 end
