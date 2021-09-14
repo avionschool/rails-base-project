7e83d81b7bcaed89247989aadc4eb5448bac6a5d
chestergarett <chestergarett@gmail.com>
2021-09-15 01:30:01 +0800

    resolving rubocop comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(*)
     dashboard_path
   end
 
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,3 @@
 class DashboardController < ApplicationController
-  
-  def index
-  end
-  
+  def index; end
 end
