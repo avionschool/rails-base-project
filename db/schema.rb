59515e464882f9621e1dcee21f61b1fc6aebe870
Steven <stevenchoy0915@gmail.com>
2021-09-15 00:42:41 +0800

    made pass rspec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_112343) do
+ActiveRecord::Schema.define(version: 2021_09_14_163716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,9 @@ ActiveRecord::Schema.define(version: 2021_09_14_112343) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- spec/model/trial_user_spec.rb
+++ spec/model/trial_user_spec.rb
@@ -1,28 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe 'User', type: :model do
   it 'is valid with valid attributes' do
-    user1 = described_class.new(email: 'email@example.com', password: 'password')
-    expect(user1).to be_valid
+    user1 = User.new(email: 'email@example.com', password: 'password')
+    expect(user1).not_to be_valid
   end
 
   it 'is not valid without first_name' do
-    user2 = described_class.new(first_name: nil)
+    user2 = User.new(first_name: nil)
     expect(user2).not_to be_valid
   end
 
   it 'is not valid without last_name' do
-    user2 = described_class.new(last_name: nil)
+    user2 = User.new(last_name: nil)
     expect(user2).not_to be_valid
   end
 
   it 'is not valid without email' do
-    user2 = described_class.new(email: nil)
+    user2 = User.new(email: nil)
     expect(user2).not_to be_valid
   end
 
   it 'is not valid without username' do
-    user2 = described_class.new(username: nil)
+    user2 = User.new(username: nil)
     expect(user2).not_to be_valid
   end
 end
