270924dbcb0ad28ea602bdf32ef10541149ec2bd
Bryan Arboleda <jbdarboleda@gmail.com>
2021-09-14 20:22:27 +0800

    Rspec model test for User and Devise setup for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :admins, path: 'admins', controllers: { sessions: "admins/sessions" }
+  devise_for :users, path: 'users', controllers: { sessions: "users/sessions" }
 
   root 'pages#home'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
+# RSpec.describe User, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    user1 = User.new(email: 'email@example.com', password: 'password')
+    expect(user1).to be_valid
+  end
+
+  it "is not valid without first_name" do
+    user2 = User.new(first_name: nil)
+    expect(user2).to_not be_valid
+  end
+
+  it "is not valid without last_name" do
+    user2 = User.new(last_name: nil)
+    expect(user2).to_not be_valid
+  end
+
+  it "is not valid without email" do
+    user2 = User.new(email: nil)
+    expect(user2).to_not be_valid
+  end
+
+  it "is not valid without username" do
+    user2 = User.new(username: nil)
+    expect(user2).to_not be_valid
+  end
+
 end
