a4f207519003bcfea2d4b14021964163dcf13f92
Brando <brandomoreno06@outlook.com>
2021-09-14 20:45:55 +0800

    added uncommited files

--- config/initializers/iex_client.rb
+++ config/initializers/iex_client.rb
@@ -2,7 +2,3 @@ IEX::Api.configure do |config|
   config.publishable_token = 'Tpk_603243bdac5b4a0887544d42868da69a' # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
   config.endpoint = 'https://sandbox.iexapis.com/v1'
 end
-
-
-# create an instance of client
-#client = IEX::Api::Client.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :stocks do
+    resources :trades, only: [:new]
+  end
+
+  resources :trades, only: [:index]
+
   root 'pages#home'
+  post '/stocks/:stock_id/trades/new' => 'trades#create', as: 'create_trade'
 end
