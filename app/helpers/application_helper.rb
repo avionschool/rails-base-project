48d97159c24cc999dbe04629dd31935c1b9970b0
dolee <dollosa.christian@gmail.com>
2021-09-16 02:06:26 +0800

    completed: user login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def render_svg(path)
-    File.open("app/assets/images/#{path}", "rb") do |file|
+    File.open("app/assets/images/#{path}", 'rb') do |file|
       raw file.read
     end
   end
