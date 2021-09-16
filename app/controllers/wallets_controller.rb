ea9c3b22d6b01a48516315236f6be5a59c3f0153
chestergarett <chestergarett@gmail.com>
2021-09-16 08:54:24 +0800

    adding wallet views

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,10 +1,36 @@
 class WalletsController < ApplicationController
+  before_action :setup, only: [:show]
+
   def index
   end
 
   def deposit
+    @deposit = Wallet.new
+  end
+  
+  def withdraw
+    @withdraw = Wallet.new
   end
 
-  def withdraw
+  def create
+    @wallet = Wallet.new(tweet_params)
+    @wallet.user = current_user
+    if @wallet.save
+      flash[:notice] = "Your transaction has been posted to your account."
+      redirect_to @wallet
+    else
+      flash[:alert] = "Invalid inputs. Please check form."
+      render :new
+    end
+  end
+
+  private
+
+  def setup
+    @wallet = Wallet.find(params[:id])
+  end
+
+  def wallet_params
+    params.require(:wallet).permit(:deposit, :withdrawals)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
   get 'dashboard', to: 'dashboard#index'
-  resources :wallets, except: [:destroy]
+  resources :wallets, except: [:destroy] do
+    get :deposit
+    get :withdraw
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
