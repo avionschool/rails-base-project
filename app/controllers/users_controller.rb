58f5f3f1fdfb7219683abccbff1449cee706abc2
dolee <dollosa.christian@gmail.com>
2021-09-17 03:43:04 +0800

    completed market index. added dummy controller, routes, just to view UI in FE

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
 class UsersController < ApplicationController
   def index; end
+  def market_index; end #delete this once market view has been created
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   resources :users
   resources :admins
   resources :portfolios, only: [:new]
-  
+
+  get '/markets', to: 'static_pages#market_index'
 
   root 'static_pages#home_page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+ 
