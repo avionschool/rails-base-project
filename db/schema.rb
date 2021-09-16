66f28c27e2b64af9a8fc34cf83c7fe42a3626425
chestergarett <chestergarett@gmail.com>
2021-09-17 01:23:49 +0800

    resolving layout differences based on latest approved PR

--- db/schema.rb
+++ db/schema.rb
@@ -19,11 +19,28 @@ ActiveRecord::Schema.define(version: 2021_09_15_153104) do
     t.string "role"
   end
 
-  create_table "user_stocks", force: :cascade do |t|
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.string "name"
+    t.decimal "last_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_roles", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "role_id", null: false
     t.index ["role_id"], name: "index_user_roles_on_role_id"
     t.index ["user_id"], name: "index_user_roles_on_user_id"
+  end
+
+  create_table "user_stocks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "stock_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["stock_id"], name: "index_user_stocks_on_stock_id"
+    t.index ["user_id"], name: "index_user_stocks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,4 +70,6 @@ ActiveRecord::Schema.define(version: 2021_09_15_153104) do
 
   add_foreign_key "user_roles", "roles"
   add_foreign_key "user_roles", "users"
+  add_foreign_key "user_stocks", "stocks"
+  add_foreign_key "user_stocks", "users"
 end
