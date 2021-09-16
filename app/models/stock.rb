8b4319a3ce71ff1903616fe670ed8913ed607d63
Carl See <carlsee06@gmail.com>
2021-09-16 20:12:31 +0800

    further fix issue in stock.rb rspec

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -9,8 +9,8 @@ class Stock < ApplicationRecord
                                     endpoint: 'https://sandbox.iexapis.com/v1')
     begin
       new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
-    rescue Exception => e
-      puts "caught exception #{e}!"
+    rescue StandardError
+      nil
     end
   end
 end
