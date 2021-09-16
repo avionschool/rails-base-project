d06b8efa3ac1e600199eb857195dbb067e9517a8
Brando <brandomoreno06@outlook.com>
2021-09-16 23:33:46 +0800

    Updated primary key of stocks

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,10 +1,10 @@
 class Trade < ApplicationRecord
   belongs_to :user
-  belongs_to :stock
+  belongs_to :stock, foreign_key: 'stock_code', inverse_of: :trades
 
   enum transaction_type: { buy: 0, sell: 1 }
   validates :user_id, presence: true
-  validates :stock_id, presence: true
+  validates :stock_code, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :quantity, presence: true, numericality: { greater_than: 0 }
   validates :transaction_type, presence: true
