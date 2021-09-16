fe2d020bc1d7ed23fa5c0bc6bdde842e283026f1
Carl See <carlsee06@gmail.com>
2021-09-16 20:48:10 +0800

    fixed issues in PR

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -9,7 +9,7 @@ class Stock < ApplicationRecord
                                     endpoint: 'https://sandbox.iexapis.com/v1')
     begin
       new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
-    rescue StandardError
+    rescue IEX::Errors::SymbolNotFoundError
       nil
     end
   end
