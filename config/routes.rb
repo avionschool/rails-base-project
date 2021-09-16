3a2ece2282b7b583725c94cdd9759a7c7ad4914b
Steven <stevenchoy0915@gmail.com>
2021-09-16 20:14:20 +0800

    deleted routes because of deleted landing page in route for Steph's PR

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :admins, path: 'admins', controllers: { sessions: "admins/sessions" }
   devise_for :users, path: 'users', controllers: { sessions: "users/sessions" }
-  root 'landing#signupUser'
+
 end
