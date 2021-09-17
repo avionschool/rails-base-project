48db85c9dcf4f7a7c98cd89d3402c973ddebc1c7
chestergarett <chestergarett@gmail.com>
2021-09-18 00:36:43 +0800

    resolving rubocop tests

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,5 +1,4 @@
 class UserRole < ApplicationRecord
   belongs_to :user
   belongs_to :role
-
 end
--- db/migrate/20210915140513_create_roles.rb
+++ db/migrate/20210915140513_create_roles.rb
@@ -2,6 +2,7 @@ class CreateRoles < ActiveRecord::Migration[6.0]
   def change
     create_table :roles do |t|
       t.string :role
+      t.index :role, unique: true
     end
   end
 end
