51320a15e8c5d95be696d9b4b689f38dae9475a5
venice0720 <vincentluague07@gmail.com>
2021-09-18 00:08:22 +0800

    Fix rubocop user model errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  
   devise :database_authenticatable, :registerable,
          :recoverable, :validatable, :rememberable, :confirmable
-  validates :username, presence: true, uniqueness: true
+  validates :username, uniqueness: true        
+  validates :username, presence: true
   validates :firstname, :lastname, presence: true, length: { maximum: 20 }
-  
 end
