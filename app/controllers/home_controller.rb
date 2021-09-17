7809dda357d7ee2655f30702228effad4c3d3ba7
stephdajon <stephdajon@gmail.com>
2021-09-18 04:31:27 +0800

    home-page: Added NavBar, UI, and partial file for landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
       secret_token: Rails.application.credentials.config[:aws][:secret_token],
       endpoint: 'https://sandbox.iexapis.com/stable'
     )
-    
+
     @crypto = @client.crypto('BTCUSDT')
     @bitcoin = @crypto.company_name
     @active = @client.stock_market_list(:mostactive)
@@ -15,7 +15,7 @@ class HomeController < ApplicationController
     # @spy = client.market['SPY'].close.price
     @fb = @client.price('FB')
     # @fb_logo = client.logo('FB')
-    @chart = @client.chart('MSFT','id', chart_simplify: true)
+    @chart = @client.chart('MSFT', 'id', chart_simplify: true)
     @logo = @client.logo('MSFT')
   end
 end
