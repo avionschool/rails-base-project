cad78080145d7eb42411813a88343af9861209c1
dolee <dollosa.christian@gmail.com>
2021-09-18 07:55:32 +0800

    re generated schema. added new lines to new files

--- db/schema.rb
+++ db/schema.rb
@@ -36,13 +36,6 @@ ActiveRecord::Schema.define(version: 2021_09_14_185348) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "sample_stocks", force: :cascade do |t|
-    t.string "mkt_symbol"
-    t.float "curr_price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
