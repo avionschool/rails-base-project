7e46bfeaa3fe6a92f7f91ef60576943b93349243
stephdajon <stephdajon@gmail.com>
2021-09-17 17:21:11 +0800

    Merge

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,18 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    client = IEX::Api::Client.new(
+      publishable_token: 'Tpk_120e7d5173ef4d039e69274c8a21bde4',
+      secret_token: 'Tsk_adbfa3c9eedc4ce8aae006027027ea78',
+      endpoint: 'https://sandbox.iexapis.com/stable'
+    )
+    @crypto = client.crypto('BTCUSDT')
+    @active = client.stock_market_list(:mostactive)
+    # @all = client.crypto('SQL')
+    # @market = client.market
+    # @spy = client.market['SPY'].close.price
+    # @fb = client.price('FB')
+    # @fb_logo = client.logo('FB')
+    # @chart = client.chart('MSFT', 'id', chart_simplify: true)
+    # @logo = client.logo('MSFT')
+  end
 end
