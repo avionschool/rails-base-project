d67338519b803138c7a4ba6efbafbeaa46f378b6
stephdajon <stephdajon@gmail.com>
2021-09-18 04:30:11 +0800

    home-page: Added NavBar, UI, and partial file for landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,21 @@
 class HomeController < ApplicationController
   def index
-    client = IEX::Api::Client.new(
-      publishable_token: Rails.config.publishable_token,
-      secret_token: Rails.config.secret_token,
+    @client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.config[:aws][:publishable_token],
+      secret_token: Rails.application.credentials.config[:aws][:secret_token],
       endpoint: 'https://sandbox.iexapis.com/stable'
     )
-    @crypto = client.crypto('BTCUSDT')
-    @active = client.stock_market_list(:mostactive)
+    
+    @crypto = @client.crypto('BTCUSDT')
+    @bitcoin = @crypto.company_name
+    @active = @client.stock_market_list(:mostactive)
+    @sectors = @client.sectors('MARKET')
     # @all = client.crypto('SQL')
-    # @market = client.market
+    @market = @client.market
     # @spy = client.market['SPY'].close.price
-    # @fb = client.price('FB')
+    @fb = @client.price('FB')
     # @fb_logo = client.logo('FB')
-    # @chart = client.chart('MSFT', 'id', chart_simplify: true)
-    # @logo = client.logo('MSFT')
+    @chart = @client.chart('MSFT','id', chart_simplify: true)
+    @logo = @client.logo('MSFT')
   end
 end
--- app/controllers/landing_controller.rb
+++
@@ -1,5 +0,0 @@
-class LandingController < ApplicationController
-  def index
-    # @categories = Category.all
-  end
-end
