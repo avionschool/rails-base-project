27a1a36cad1681e359b1115abf1993070af6ea45
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-19 01:50:59 +0800

    fixed rubocop

--- app/helpers/markets_helper.rb
+++ app/helpers/markets_helper.rb
@@ -1,9 +1,8 @@
 module MarketsHelper
-  
-  def calculate_price(iex_price, bool) 
+  def calculate_price(iex_price, bool)
     usd_to_php = 50.06
     convert_price = iex_price * usd_to_php
     with_interest_total = bool ? convert_price + (convert_price * 0.05) : convert_price - (convert_price * 0.05)
     number_to_currency(with_interest_total, unit: 'PHP ')
-  end 
+  end
 end
