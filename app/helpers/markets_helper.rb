bbd5d4beec58f4f7d8eca2551063ac7b505e0b31
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-19 01:48:57 +0800

    improved helper method to avoid DRY

--- app/helpers/markets_helper.rb
+++ app/helpers/markets_helper.rb
@@ -1,15 +1,9 @@
 module MarketsHelper
-  def current_buy_price(iex_price)
+  
+  def calculate_price(iex_price, bool) 
     usd_to_php = 50.06
     convert_price = iex_price * usd_to_php
-    with_interest_total = convert_price + (convert_price * 0.05)
+    with_interest_total = bool ? convert_price + (convert_price * 0.05) : convert_price - (convert_price * 0.05)
     number_to_currency(with_interest_total, unit: 'PHP ')
-  end
-
-  def current_sell_price(iex_price)
-    usd_to_php = 50.06
-    convert_price = iex_price * usd_to_php
-    with_interest_total = convert_price - (convert_price * 0.05)
-    number_to_currency(with_interest_total, unit: 'PHP ')
-  end
+  end 
 end
