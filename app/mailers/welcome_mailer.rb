fdc95631b4af6c30cc318b9517aa526dde52a1cb
dolee <dollosa.christian@gmail.com>
2021-09-18 23:47:51 +0800

    reconfigured action mailer for welcome email. re designed welcome email template

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,5 +1,5 @@
 class WelcomeMailer < ApplicationMailer
-  def welome_account_email
-    mail(to: 'emporia.wallet@gmail.com', subject: 'Welome to Emporia Wallet!')
+  def welome_account_email(email)
+    mail(to: email, subject: 'Welome to Emporia Wallet!')
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  after_create :send_welcome_email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +7,10 @@ class User < ApplicationRecord
 
   validates :username, presence: true
   validates :full_name, presence: true
+
+  private
+
+  def send_welcome_email
+    WelcomeMailer.welome_account_email(email).deliver_now
+  end
 end
