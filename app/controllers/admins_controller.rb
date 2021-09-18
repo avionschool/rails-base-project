f36cc0eed2272d2929093be54de8bcdbe8c45cbc
dolee <dollosa.christian@gmail.com>
2021-09-19 03:41:04 +0800

    added new views for admin panel. added user seeds

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,3 +1,11 @@
 class AdminsController < ApplicationController
-  def index; end
+  def trader_settings
+    @traders = User.all
+  end
+
+  def approvals
+    @traders = User.all
+  end
+
+  # def transactions; end #uncomment once transaction models has been created
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def append_admin_style_parent_container(curr_class)
+    request.fullpath.start_with?('/admins/') && admin_signed_in? == false ? "#{curr_class} admin" : curr_class.to_s
+  end
+
   def append_admin_style(curr_class)
     request.fullpath.start_with?('/admins/') ? "#{curr_class} admin" : curr_class.to_s
   end
