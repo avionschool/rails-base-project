a9574b774ac378e0c81e002ece2cf7631eb0cceb
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-19 01:05:53 +0800

    added helper method for conversion of usd to php

---
+++ app/helpers/markets_helper.rb
@@ -0,0 +1,16 @@
+module MarketsHelper
+
+  def current_buy_price (iex_price)
+    usd_to_php = 50.06
+    convert_price = iex_price * usd_to_php
+    with_interest_total = convert_price + (convert_price * 0.05)
+    return number_to_currency(with_interest_total, :unit => "PHP ")
+  end   
+
+    def current_sell_price (iex_price)
+    usd_to_php = 50.06
+    convert_price = iex_price * usd_to_php
+    with_interest_total = convert_price - (convert_price * 0.05)
+    return number_to_currency(with_interest_total, :unit => "PHP ")
+  end 
+end
