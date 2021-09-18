5d66f5516c81e312839e7d9f572b860848524c70
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-19 01:18:12 +0800

    fixed rubocop

--- app/helpers/markets_helper.rb
+++ app/helpers/markets_helper.rb
@@ -1,16 +1,15 @@
 module MarketsHelper
-
-  def current_buy_price (iex_price)
+  def current_buy_price(iex_price)
     usd_to_php = 50.06
     convert_price = iex_price * usd_to_php
     with_interest_total = convert_price + (convert_price * 0.05)
-    return number_to_currency(with_interest_total, :unit => "PHP ")
-  end   
+    number_to_currency(with_interest_total, unit: 'PHP ')
+  end
 
-    def current_sell_price (iex_price)
+  def current_sell_price(iex_price)
     usd_to_php = 50.06
     convert_price = iex_price * usd_to_php
     with_interest_total = convert_price - (convert_price * 0.05)
-    return number_to_currency(with_interest_total, :unit => "PHP ")
-  end 
+    number_to_currency(with_interest_total, unit: 'PHP ')
+  end
 end
