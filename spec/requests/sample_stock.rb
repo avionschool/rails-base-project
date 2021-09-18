4d108a91f7bdfea2a29805d751bdcc30ac762da0
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-18 18:02:14 +0800

    fixed rubocop

--- spec/requests/sample_stock.rb
+++ spec/requests/sample_stock.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe 'IEX cleint API', type: request do
-  it "returns all stock symboels" do
+describe 'StockSample', type: request do
+  it 'returns all stock symboels' do
     get 'https://sandbox.iexapis.com/v1'
 
     expect(response).to have_http_status(:success)
