5d8df3adce67bff748a078545a86950090d9c719
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-20 20:32:42 +0800

    deleted the sample_stock model

--- spec/requests/sample_stock.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe 'SampleStock', type: request do
-  it 'returns all stock symboels' do
-    get 'https://sandbox.iexapis.com/v1'
-
-    expect(response).to have_http_status(:success)
-  end
-end
