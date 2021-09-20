439db3d5941e4b86032bf0a132e4a2b23791961b
mumble-07 <andreapauline.deguzman@gmail.com>
2021-09-20 21:19:40 +0800

    review changed, deleted the uncessary comments

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,17 +3,5 @@ class PortfoliosController < ApplicationController
     @users = User.all
     @markets = Market.all
     @portfolios = Portfolio.all
-
-    # @portfolio = @sample_stocks.portfolios.build
-  end
-
-  def create
-    @portfolios = @sample_stocks.portfolios.build(portfolio_params)
-  end
-
-  private
-
-  def portfolio_params
-    params.require(:portfolio).permit(:user_id, :market_symbol, :hist_price, :amount, :sample_stock_id)
-  end
+   end
 end
