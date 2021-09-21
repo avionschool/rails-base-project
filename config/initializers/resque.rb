d849aaebe9472a5dd5e6d5f532e90631a838a401
dolee <dollosa.christian@gmail.com>
2021-09-22 00:49:33 +0800

    confugured resque initializer to consider production env redis var

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,1 +1,10 @@
+require 'resque/server'
+
 Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
+
+if Rails.env.development?
+  Resque.redis = Redis.new(host: 'localhost', port: '6379')
+else
+  uri = URI.parse(ENV['REDIS_URL'])
+  Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+end
