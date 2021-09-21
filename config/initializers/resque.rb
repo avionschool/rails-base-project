280b8495448a16ba4d59d63a21d975eb7a5ea668
dolee <dollosa.christian@gmail.com>
2021-09-22 05:42:42 +0800

    changed local port for redis

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,7 +2,7 @@ require 'resque/server'
 require 'uri'
 
 if Rails.env.development?
-  Resque.redis = Redis.new(host: 'localhost', port: '1231')
+  Resque.redis = Redis.new(host: 'localhost', port: '6379')
 else
   uri = URI.parse(Rails.application.credentials.redis_heroku[:REDIS_URL])
   Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
