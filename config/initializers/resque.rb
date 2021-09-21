2238f41c2b13ae64418bd08acc4754c103acf187
dolee <dollosa.christian@gmail.com>
2021-09-22 01:11:29 +0800

    converted uri param to string

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,10 +1,13 @@
 require 'resque/server'
+require 'uri'
 
 Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
 
 if Rails.env.development?
   Resque.redis = Redis.new(host: 'localhost', port: '6379')
 else
-  uri = URI.parse(ENV['REDIS_URL'])
+  uri = URI.parse(URI.encode((ENV['REDIS_URL']).to_s))
+  # Resque.redis = Redis.new(host: 'localhost', port: '6379')
   Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+  
 end
