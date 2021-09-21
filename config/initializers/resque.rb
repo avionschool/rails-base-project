784df623e8d070619b6e02fb1b15edeea9982a7f
dolee <dollosa.christian@gmail.com>
2021-09-22 02:32:20 +0800

    added redis gem. reverted redis.rb to prev version

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -6,7 +6,6 @@ Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
 if Rails.env.development?
   Resque.redis = Redis.new(host: 'localhost', port: '6379')
 else
-  uri = URI.parse(Rails.application.credentials.redis_heroku[:REDIS_URL].to_s)
+  uri = URI.parse(Rails.application.credentials.redis_heroku[:REDIS_URL])
   Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-  
 end
