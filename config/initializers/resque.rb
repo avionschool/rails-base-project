7999aaa686446dc90f718947a8fa87f6ebf86aec
dolee <dollosa.christian@gmail.com>
2021-09-22 01:24:24 +0800

    added redis.rb based on heroku redis docs

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -7,7 +7,6 @@ if Rails.env.development?
   Resque.redis = Redis.new(host: 'localhost', port: '6379')
 else
   uri = URI.parse(URI.encode((ENV['REDIS_URL']).to_s))
-  # Resque.redis = Redis.new(host: 'localhost', port: '6379')
   Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
   
 end
