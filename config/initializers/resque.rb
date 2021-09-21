2a4086a63f0c8a1f0046a52b3050a2fe3e220e08
dolee <dollosa.christian@gmail.com>
2021-09-22 05:35:45 +0800

    removed redis.rb in initializer

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,11 +1,15 @@
 require 'resque/server'
+require 'resque'
 require 'uri'
 
 Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
 
 if Rails.env.development?
-  Resque.redis = Redis.new(host: 'localhost', port: '6379')
-else
+  puts "resque development"
+  Resque.redis = Redis.new(host: 'localhost', port: '1231')
+end
+if Rails.env.production?
+  puts "resque production"
   uri = URI.parse(Rails.application.credentials.redis_heroku[:REDIS_URL])
   Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 end
