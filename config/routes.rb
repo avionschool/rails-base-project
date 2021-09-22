a1837f79fc0fd9a871b8616907e8a967c043a30b
jeraldbatal22 <btljrld22@gmail.com>
2021-09-22 21:18:11 +0800

    Admin Features, UI and functionaility users_list, pending_user list, approve user list, transaction history, add, edit and showing user

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,21 @@ Rails.application.routes.draw do
     post '/signup',         to: 'users/registrations#create',  as: 'signup_create'
     delete '/signout',      to: 'devise/sessions#destroy',      as: 'signout'
   end
- 
+  
+  # ADMIN ONLY
+
+  get '/admin/dashboard', to: "admin#dashboard", as: 'dashboard_admin'
+  get '/admin/pending_user', to: "admin#pending_users", as: 'pending_users'
+  get '/admin/user_list', to: "admin#user_list", as: 'user_list'
+  patch '/admin/dashboard/:id', to: 'admin#approval', as: 'approve_trader'
+  delete '/admin/dashboard/:id', to: 'admin#remove_trader', as: 'remove_trader'
+  get '/admin/transaction_history', to: "admin#transactions", as: 'admin_transaction_history'
+  get 'admin/new', to: 'admin#new', as: 'new_user'
+  post 'admin/new', to: 'admin#create', as: 'create_user'
+  get 'admin/edit/:id', to: 'admin#edit', as: 'edit_user'
+  patch 'admin/update/:id', to: 'admin#update', as: 'update_user'
+  get 'admin/show/:id', to: 'admin#show', as: 'show_user'
+
   get '/dashboard', to: "home#dashboard"
   root to: "home#index"
 end
