e459103c6893892f46a0da963a4fd55fa8b59bbe
Brando <brandomoreno06@outlook.com>
2021-09-22 12:26:26 +0800

    Fixes IEX cofing endpoint to production, Updates seeds to fetch all stocks list

--- config/initializers/iex_client.rb
+++ config/initializers/iex_client.rb
@@ -1,5 +1,5 @@
 IEX::Api.configure do |config|
   config.publishable_token = ENV['IEX_API_PUBLISHABLE_TOKEN']
   config.secret_token = ENV['IEX_API_SECRET_TOKEN']
-  config.endpoint = 'https://sandbox.iexapis.com/v1'
+  config.endpoint = 'https://cloud.iexapis.com/v1'
 end
