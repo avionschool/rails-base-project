f1eb8e744e0be93916120c81acbbfe04c97f0783
Bryan Arboleda <jbdarboleda@gmail.com>
2021-09-23 01:00:13 +0800

    Completed Admin approve, create, edit and show Users

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
 
   get 'admins/home' => 'admin_portals#index'
   put 'admins/manage' => 'admin_portals#approve_user'
-  get 'admins/manage' => 'admin_portals#edit_user'
-  put 'admins/manage' => 'admin_portals#update_user'
   get 'admins/view' => 'admin_portals#show_user'
+  get 'admins/edit_user/:id/edit' => 'admin_portals#edit_user', as: 'edit_user'
+  put 'admins/update_user/:id' => 'admin_portals#update_user'
+  patch 'admins/update_user/:id' => 'admin_portals#update_user', as: 'update_user'
+  get 'admins/add_user' => 'admin_portals#add_user'
+  post 'admins/create_user' => 'admin_portals#create_user'
 
   resources :trades, only: [:index]
 
