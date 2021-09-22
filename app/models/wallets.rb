7ba948d8caa4d9d905d71050e396dae42e6a708e
Warda Nur <wardanur04@gmail.com>
2021-09-22 16:17:37 +0800

    Wallet: created model, controller, UI for wallet and fixed rubocop error

--- app/models/wallets.rb
+++ app/models/wallets.rb
@@ -1,15 +1,15 @@
 class Wallet < ApplicationRecord
   belongs_to :user
 
-  def self.total_deposits
-    sum(:deposit)
-    # if transaction_type == "credit"
-    #   @user.update!(balance: @user.balance + @amount)
-    #    elsif transaction_type == "debit"
-    #   @user.update!(balance: @user.balance - @amount)
-  end
+  # def self.total_deposits
+  #   sum(:deposit)
+  # if transaction_type == "credit"
+  #   @user.update!(balance: @user.balance + @amount)
+  #    elsif transaction_type == "debit"
+  #   @user.update!(balance: @user.balance - @amount)
+  # end
 
-  def self.total_balance
-    total_deposits - total_withdrawals
-  end
+  # def self.total_balance
+  #   total_deposits - total_withdrawals
+  # end
 end
