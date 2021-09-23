90b79aa7c65d7d7040cf9b9bf1a0278428e44a22
stephdajon <stephdajon@gmail.com>
2021-09-23 21:31:54 +0800

    resolved conflict on credentials.yml.enc

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,8 +4,12 @@ class Stock < ApplicationRecord
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex_client[:publishable_token],
       secret_token: Rails.application.credentials.iex_client[:secret_token],
-      endpoint: 'https://sandbox.iexapis.com/v1')
-    client.price(stock_symbol)
+      endpoint: 'https://sandbox.iexapis.com/stable')
+    begin  
+      new(stock_symbol: stock_symbol, logo: client.company(stock_symbol).company_name, current_price: client.price(stock_symbol))
+    rescue => exception
+      return nil
+    end    
   end
 
 end
