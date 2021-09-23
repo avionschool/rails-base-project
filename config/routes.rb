4a3922d9a3ac3ad07bca15a1fb3a9ed3bdac2953
stephdajon <stephdajon@gmail.com>
2021-09-23 16:32:26 +0800

    API: removed imported bootstrap at application scss, put sample title at application html, deleted stock market view link at home header, render partial search file from portfolio, added UI on sign up and sign in forms, and added routes for stock controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,7 @@ Rails.application.routes.draw do
     get 'homepage', to: 'portfolios#index'
   end
    
-  resources :stocks, only: [:index]
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'search_stock', to: 'stocks#search'
 
 
   #routes for admin only
