b75b4e30713dfdb0f3d73ea779d12e49257c87c5
Steven <stevenchoy0915@gmail.com>
2021-09-23 21:03:07 +0800

    hotfix on production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => Rails.application.credentials.gmail_mailer[:username],
+    :username            => Rails.application.credentials.gmail_mailer[:username],
     :password             => Rails.application.credentials.gmail_mailer[:password],
     :authentication       => "plain",
     :enable_starttls_auto => true
