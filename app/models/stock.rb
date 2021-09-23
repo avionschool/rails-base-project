fcdc23d568e9c7da82e4eabfdd26aece3dbc592b
stephdajon <stephdajon@gmail.com>
2021-09-23 22:12:32 +0800

    resolved conflicts on credentials file

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,8 @@
 class Stock < ApplicationRecord
   def self.new_lookup(stock_symbol)
     client = IEX::Api::Client.new(
-      publishable_token: Rails.application.credentials.iex_client[:publishable_token],
-      secret_token: Rails.application.credentials.iex_client[:secret_token],
+      publishable_token: Rails.application.credentials.aws[:publishable_token],
+      secret_token: Rails.application.credentials.aws[:secret_token],
       endpoint: 'https://sandbox.iexapis.com/stable'
     )
 
