26db4dfdb6a301d6fc90b755c4184a2808d7979f
stephdajon <stephdajon@gmail.com>
2021-09-25 04:06:04 +0800

    user_stock: I change the current users_homepage_path to my_portfolio_path on application controller, define search method to fetch from API on stock controller, and define my_portfolio method to track current users stocks

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,22 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_lookup(params[:stock])
-    render 'portfolios/index'
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        respond_to do |format|
+          format.js { render partial: 'users/search' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = 'Please enter a valid symbol to search'
+          format.js { render partial: 'users/search' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = 'Please enter a symbol to search'
+        format.js { render partial: 'users/search' }
+      end
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
 class UsersController < ApplicationController
-  def index; end
+  def my_portfolio
+    @tracked_stocks = current_user.stocks
+  end
 end
