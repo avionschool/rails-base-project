d7cbc241d50b991e19a7acc6edaed1c66c0a19de
Steven <stevenchoy0915@gmail.com>
2021-09-25 09:39:16 +0800

    commented validation for logo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :user_stocks, dependent: :restrict_with_exception
   has_many :stocks, through: :user_stocks
 
-  validates :logo, :stock_symbol, :current_price, presence: true #have to make a rspec test for this
+  #validates :logo, :stock_symbol, :current_price, presence: true #have to make a rspec test for this
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
