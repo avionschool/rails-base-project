6b59aaae88e09034f1462928fce4e953a2dfdc5d
Bryan Arboleda <jbdarboleda@gmail.com>
2021-10-13 23:50:01 +0800

    Created model testing for Admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,6 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true
+  validates :username, length: { minimum: 6 }
 end
