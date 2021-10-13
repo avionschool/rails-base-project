02714c33572fb8401d7823989d13143e7eb1e277
Bryan Arboleda <jbdarboleda@gmail.com>
2021-10-14 01:19:03 +0800

    added validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true
+  validates :username, length: { minimum: 6 }
 
   enum status: { free: 0, paid: 1 }
 end
