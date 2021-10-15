81f06d9ef1086cd976827a866581fbcc98788e3f
Bryan Arboleda <jbdarboleda@gmail.com>
2021-10-16 06:50:05 +0800

    Argh! Fixed rubocop error

--- app/services/seeds_services/create_admin.rb
+++ app/services/seeds_services/create_admin.rb
@@ -4,7 +4,7 @@ module SeedsServices
       @logger.info 'Processing Admin seed file'
       Admin.destroy_all
       @logger.info 'Deleted all data from Admin model'
-      Admin.create(email: "abc.dev@tech.com", password: "test1234", username: "abcdev")
+      Admin.create(email: 'abc.dev@tech.com', password: 'test1234', username: 'abcdev')
       @logger.info 'admin data created'
     end
   end
