20d0626850b3a9d69aafca309b5b44f1361855c3
mumble-07 <andreapauline.deguzman@gmail.com>
2021-10-16 06:04:54 +0800

    A/ added seeds services for user and mainbook entries

--- app/services/seeds_services/create_users.rb
+++ app/services/seeds_services/create_users.rb
@@ -6,7 +6,7 @@ module SeedsServices
       @logger.info 'Deleted all data from User model'
       user_count = 0
       10.times do
-        User.create(email: "dummy_user+#{user_count}@abc.com", encrypted_password: 'abc12345', full_name: "abc_user_ #{user_count}", username: "abc_ #{user_count}", company: 'ABC Tech Corporation')
+        User.create(email: "dummy_user+#{user_count}@abc.com", password: "abc12345#{user_count}", full_name: "abc_user#{user_count}", username: "abctech_#{user_count}", company: "ABC Tech Corporation#{user_count}")
         user_count += 1
       end
       @logger.info '10 user data created'
