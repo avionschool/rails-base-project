57309a3cf16500a0a49a6ac448634a0b63eaaba3
mumble-07 <andreapauline.deguzman@gmail.com>
2021-10-16 06:05:47 +0800

    A/ schema update

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_10_15_112205) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
+      t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "full_name"
     t.string "username"
