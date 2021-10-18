191a27987aa81e04af602f6d4388b710d14bba28
mumble-07 <andreapauline.deguzman@gmail.com>
2021-10-19 04:07:46 +0800

    controller CRUD for minibooks

--- app/controllers/mini_books_controller.rb
+++ app/controllers/mini_books_controller.rb
@@ -1,39 +1,53 @@
 class MiniBooksController < ApplicationController
-  def new
-    @mini_book = MiniBook.new
-  end
-
   def index
     @mini_books = MiniBook.all
   end
 
-  def show; end
+  def new
+    @mini_book = MiniBook.new
+  end
 
   def create
     @mini_book = MiniBook.new(mini_book_params)
     if @mini_book.save
-      redirect_to mini_books_path, notice: 'Product was successfully created.'
+      redirect_to minibooks_path, notice: 'Product was successfully created.'
     else
-      render 'new'
+      render :new
     end
+  end
+
+  def edit
+    @mini_book = MiniBook.find(params[:id])
   end
 
   def update
+    @mini_book = MiniBook.find(params[:id])
     if @mini_book.update(mini_book_params)
-      redirect_to @mini_book, notice: 'Product was successfully updated.'
+      flash[:notice] = 'Sucessfully updated MiniBook'
+      redirect_to minibooks_path
     else
-      render :edit, status: :unprocessable_entity
+      render :edit
     end
   end
 
-  def destroy
-    @mini_book.destroy
-    redirect_to mini_books_path, notice: 'Product was successfully destroyed.'
-  end
+  # def show; end
+
+  # def update
+  #   if @mini_book.update(mini_book_params)
+  #     redirect_to @mini_book, notice: 'Product was successfully updated.'
+  #   else
+  #     render :edit, status: :unprocessable_entity
+  #   end
+  # end
+
+  # def destroy
+  #   @mini_book.destroy
+  #   redirect_to mini_books_path, notice: 'Product was successfully destroyed.'
+  # end
 
   private
 
   def mini_book_params
-    params.require(:mini_book).permit(:minibook_name, :minibook_price)
+    params.require(:mini_book).permit(:user_id, :minibook_name, :minibook_price)
   end
 end
