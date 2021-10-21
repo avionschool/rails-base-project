07c43c475f10ad1ed939f159ecb888fb3d7a9a19
Bryan Arboleda <jbdarboleda@gmail.com>
2021-10-21 19:57:38 +0800

    B/setup devise mailer for User registration, added validation for User model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.perform_deliveries = true
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -59,8 +63,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  # Action mailer 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
 end
