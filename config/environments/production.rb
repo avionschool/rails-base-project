764752ec862c0bca177e9db50a9abe798f69b8f8
Bryan Arboleda <jbdarboleda@gmail.com>
2021-10-21 20:11:20 +0800

    removed smtp settings to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,14 +111,6 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.default_url_options = { host: 'https://phinance-io-abc-tech.herokuapp.com/' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => ENV["GMAIL_USERNAME"],
-    :password => ENV["GMAIL_PASSWORD"],
-    :authentication => "plain",
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.perform_deliveries = true
 
 end
