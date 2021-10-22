e674ecf1dbafe3189a1b6a876b79ce7a450a0896
mumble-07 <andreapauline.deguzman@gmail.com>
2021-10-22 20:30:57 +0800

    rspec model test for minibook entries

--- app/models/minibook_entry.rb
+++ app/models/minibook_entry.rb
@@ -1,2 +1,5 @@
 class MinibookEntry < ApplicationRecord
+  validates :date_minib_entry, presence: true
+  validates :minibook_entry_description, presence: true
+  validates :or_vat_reg_tin_mainib, presence: true
 end
