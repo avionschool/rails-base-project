da52817cb9f04997630b2a4bd0e530b9133eef5b
mumble-07 <andreapauline.deguzman@gmail.com>
2021-10-23 04:51:13 +0800

    A/ set up data factories using factory bot

---
+++ spec/factories.rb
@@ -0,0 +1,48 @@
+FactoryBot.define do
+  factory :user do
+    sequence(:email) { |e| "dummy_user+#{e}@abc.com" }
+    sequence(:password) { |pw| "abc12345#{pw}" }
+    sequence(:full_name) { |fn| "abc_user#{fn}" }
+    sequence(:username) { |un| "abctech_#{un}" }
+    sequence(:company) { |comp| "ABC Tech Corporation #{comp}" }
+    # user.skip_confirmation!
+    # user.save!
+  end
+
+  factory :user_pending, class: 'User' do
+    sequence(:email) { |e| "dummy_user+#{e}@abc.com" }
+    sequence(:password) { |pw| "abc12345#{pw}" }
+    sequence(:full_name) { |fn| "abc_user#{fn}" }
+    sequence(:username) { |un| "abctech_#{un}" }
+    sequence(:company) { |comp| "ABC Tech Corporation #{comp}" }
+  end
+
+  factory :admin do
+    email { 'abc.dev@tech.com' }
+    password { 'test1234' }
+    username { 'abcdev' }
+  end
+
+  factory :mainbook_entries, class: 'MainBook' do
+    date_mainb { DateTime.now }
+    mainbook_description { 'Payment for something' }
+    or_vat_reg_tin_mainb { 'tin12345' }
+    debit_mainb { rand(100..5000) }
+    credit_mainb { rand(100..5000) }
+    balance_mainb { rand(100..5000) }
+  end
+
+  factory :create_minibook, class: 'MiniBook' do
+    user_id { rand(1..100) }
+    minibook_name { 'Client Avion' }
+  end
+
+  factory :minibook_entries, class: 'MinibookEntry' do
+    date_minib_entry { DateTime.now }
+    minibook_entry_description { 'Electronics stuff' }
+    or_vat_reg_tin_mainib { 'tin12345' }
+    debit_minib { rand(100..5000) }
+    credit_minib { rand(100..5000) }
+    minibook_balance { rand(100..5000) }
+  end
+end
