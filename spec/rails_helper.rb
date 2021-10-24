0355e6d8b78afa3f463f7669f61592e8f15e6d8b
mumble-07 <andreapauline.deguzman@gmail.com>
2021-10-24 21:04:06 +0800

    added require devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'devise'
 require 'shoulda/matchers'
 require 'factory_bot_rails'
 
