fcc9cd9af2ee47636d8263e2ffd63a0cabc584cc
mumble-07 <andreapauline.deguzman@gmail.com>
2021-10-24 17:48:28 +0800

    request test for mainbook controller

--- spec/requests/main_books_controllers_spec.rb
+++ spec/requests/main_books_controllers_spec.rb
@@ -1,25 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'MainBooks', type: :request do
-  # before(:create, &:skip_confirmation!)
   let(:confirmed_user) { create(:confirmed_user) }
   let(:user_pending) { create(:user_pending) }
   let(:mainbook_entries) { create(:mainbook_entries) }
 
-  describe 'GET /main_books_controllers' do
-    it 'returns the index page' do
+  describe 'user pending cannot access mainbook page' do
+    before do
+      sign_in user_pending
+    end
+
+    it 'returns to the index page' do
       get root_path
       expect(response).to have_http_status(:ok)
     end
   end
 
-  describe 'User gets to access mainbook page' do
+  describe 'confirmed user gets to access mainbook page' do
     before do
       sign_in confirmed_user
     end
 
     it 'returns the mainbook entries page' do
       get mainbook_entries_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'User gets to create a mainbook entry' do
+    before do
+      sign_in confirmed_user
+      create(:mainbook_entries)
+    end
+
+    it 'direct users to main book entry form' do
+      get mainbook_new_path
       expect(response).to have_http_status(:ok)
     end
   end
