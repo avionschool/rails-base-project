bdcf6ed7ce26d715aa12fec318b73543de78e023
mumble-07 <andreapauline.deguzman@gmail.com>
2021-10-26 14:25:00 +0800

    fixed rename of or vat reg tin column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_23_024605) do
+ActiveRecord::Schema.define(version: 2021_10_25_205056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 2021_10_23_024605) do
     t.integer "mini_book_id"
     t.datetime "date_minib_entry"
     t.string "minibook_entry_description"
-    t.string "or_vat_reg_tin_mainib"
+    t.string "or_vat_reg_tin_minib"
     t.float "debit_minib"
     t.float "credit_minib"
     t.float "minibook_balance"
@@ -84,10 +84,10 @@ ActiveRecord::Schema.define(version: 2021_10_23_024605) do
     t.integer "minibook_id"
     t.integer "status", default: 0
     t.string "company"
-    t.string "plan"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "plan"
     t.string "stripe_customer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
