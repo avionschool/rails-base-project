8491e85483dbfd8abf9b01c550681632d6b7f646
TanTanMa <gelatan12@gmail.com>
2021-12-04 11:06:16 +0800

    added has_secure_pw

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
             length: {maximum: 105},
             uniqueness: {case_sensitive: false},
             format: {with: VALID_EMAIL_REGEX}
+    has_secure_password
 end
