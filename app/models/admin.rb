c30b016c580ffb4123238d88b50d7bcb61ee7886
almedaria <rmmgalmeda@gmail.com>
2021-12-04 12:40:18 +0800

    added admin validations

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,9 @@
 class Admin < ApplicationRecord
+    validates :username, uniqueness:true, presence:true
+    validates :fullname, presence:true, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP , message:"enter a valid email address"} , presence:true
+    validates :password, length: { minimum: 6 , message: "minimum 6 characters"}, confirmation: true
+    validates :password_confirmation, presence:true
+    #add to forms <%= text_field :admin, :password_confirmation %>
 end
