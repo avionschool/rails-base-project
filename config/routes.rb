303b3255abb1c58b2d44d59e937ca98122f60a5e
ralph <ralphlsulit@gmail.com>
2021-12-06 15:23:32 +0800

    added create user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
-  get 'signup', to: 'users#new'
+
+  #User routes
+  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+
+  #Session routes
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 end
