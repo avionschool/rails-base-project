61e083a59a08d9fd6c4106bafc10345656d66d35
TanTanMa <gelatan12@gmail.com>
2021-12-09 14:20:50 +0800

    generated users model via Device

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,6 @@
 class User < ApplicationRecord
-    before_save {self.email = email.downcase }
-    validates :username, presence: true, 
-            uniqueness: {case_sensitive: false}, 
-            length: {minimum: 3, maximum: 25}
-
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-    validates :email, presence: true, 
-            length: {maximum: 105},
-            uniqueness: {case_sensitive: false},
-            format: {with: VALID_EMAIL_REGEX}
-    has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
