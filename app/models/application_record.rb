82cad1bb590b2757972f7be35dc13f7a49ba67e9
Richelle Lobo <lobo.r0018@gmail.com>
2021-12-10 09:37:11 +0800

    Sort records by date of creation instead of primary key

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  # Sort records by date of creation instead of primary key
+  self.implicit_order_column = :created_at
 end
