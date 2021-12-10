3b1da92c5a01562485bb457cf39de0f446dfc037
Richelle Lobo <lobo.r0018@gmail.com>
2021-12-10 09:36:48 +0800

    Creates migration to use uuid instead of id to tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_04_022957) do
+ActiveRecord::Schema.define(version: 2021_12_10_013138) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
