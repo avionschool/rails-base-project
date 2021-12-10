645fbf43d98689671cbd808ec3e91030da04299d
ccases <carlosmatthewcases@gmail.com>
2021-12-10 17:33:12 +0800

    added kind (buy or sell) to orders, forgot in first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_090132) do
+ActiveRecord::Schema.define(version: 2021_12_10_093221) do
 
   create_table "coins", force: :cascade do |t|
     t.string "base"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_12_10_090132) do
     t.integer "coin_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "kind"
     t.index ["coin_id"], name: "index_orders_on_coin_id"
     t.index ["wallet_id"], name: "index_orders_on_wallet_id"
   end
