ef1bb73f9cb3244024974dece0156b1615ee044e
TanTanMa <gelatan12@gmail.com>
2021-12-11 11:42:02 +0800

    changed INTERGER to INTEGER

--- db/migrate/20211211031406_create_stocks.rb
+++ db/migrate/20211211031406_create_stocks.rb
@@ -2,7 +2,7 @@ class CreateStocks < ActiveRecord::Migration[6.0]
   def change
     create_table :stocks do |t|
       t.string :ticker
-      t.interger :user_id
+      t.integer :user_id
 
       t.timestamps
     end
