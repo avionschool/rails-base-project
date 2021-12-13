7193188030469f406cefcfa5671316a8f13dfbc4
Richelle Lobo <lobo.r0018@gmail.com>
2021-12-13 14:44:04 +0800

    Add migration for account model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_063812) do
+ActiveRecord::Schema.define(version: 2021_12_13_064124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "accounts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "img"
+    t.string "role"
+    t.string "firstname"
+    t.string "lastname"
+    t.text "address"
+    t.string "contact_number"
+    t.boolean "is_verified"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
