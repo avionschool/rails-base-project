46a00c76e03257311fbd8765909591bad564e35b
Richelle Lobo <lobo.r0018@gmail.com>
2021-12-13 18:26:02 +0800

    Adds carrierwave gem for file uploading

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'hamlit-rails'
 gem 'pry'
+gem 'carrierwave', '~> 2.0'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,6 @@
 class Account < ApplicationRecord
+	mount_uploader :img, AvatarUploader
 	has_one :user
   	accepts_nested_attributes_for :user
-  	enum role: { admin: "admin", broker: "broker", trader: "trader"}
+  	enum roles: { admin: "admin", broker: "broker", trader: "trader"}
 end
