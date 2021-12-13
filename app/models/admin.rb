435c4aad7f74b5ed5b733f1945422d1db805716c
jujubean22 <jmevillanueva22@gmail.com>
2021-12-13 15:49:34 +0800

    added validations and relationships on buyer, broker and admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,8 @@
 class Admin < ApplicationRecord
+  has_many :brokers
+  has_many :buyers
+  validates :email, presence: true
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable, :timeoutable
 end
