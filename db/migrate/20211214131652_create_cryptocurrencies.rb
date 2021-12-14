aad33a0ea4c75549fdc04f18614e352bd833640b
JM Arenas <rns.jey@gmail.com>
2021-12-14 21:21:30 +0800

    Redefine cryptocurrency table
    Removed description column
    Changed currency_symbol to symbol

--- db/migrate/20211214131652_create_cryptocurrencies.rb
+++ db/migrate/20211214131652_create_cryptocurrencies.rb
@@ -2,9 +2,8 @@ class CreateCryptocurrencies < ActiveRecord::Migration[6.0]
   def change
     create_table :cryptocurrencies do |t|
       t.string :name
-      t.string :description
       t.bigint :max_supply
-      t.string :currency_symbol
+      t.string :symbol
       t.string :slug
       t.boolean :is_active
 
