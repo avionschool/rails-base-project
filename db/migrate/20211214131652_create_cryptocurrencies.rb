a6040b0cc3a1319e90f345f7c1aa27bc46004143
JM Arenas <rns.jey@gmail.com>
2021-12-14 21:17:59 +0800

    Populate cryptocurrency table columns

--- db/migrate/20211214131652_create_cryptocurrencies.rb
+++ db/migrate/20211214131652_create_cryptocurrencies.rb
@@ -1,6 +1,12 @@
 class CreateCryptocurrencies < ActiveRecord::Migration[6.0]
   def change
     create_table :cryptocurrencies do |t|
+      t.string :name
+      t.string :description
+      t.bigint :max_supply
+      t.string :currency_symbol
+      t.string :slug
+      t.boolean :is_active
 
       t.timestamps
     end
