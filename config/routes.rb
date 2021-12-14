61502b4a49c1f8aa17aa1345b9a569e8bf97c1aa
Kobato13 <johncarlomartinez13@gmail.com>
2021-12-14 16:44:24 +0800

    Added devise for user login, register, signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  get 'home/index'
+  root to: "home#index"
   get "/test", to: "test#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
