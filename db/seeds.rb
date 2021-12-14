06bae0ef3f5e604b1833a9759348e45adc802c19
JM Arenas <rns.jey@gmail.com>
2021-12-14 21:40:45 +0800

    Created db seed for sample cryptocurrencies

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# Author: github.com/rns.jey
 #
-# Examples:
+# This seed file contains a list of data for 10 unique cryptocurrencies
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Column fields:
+# @name [String] the name of the currency
+# @max_supply [BigInt] the max_supply for the currency
+# @symbol [String] the symbol for the crypto currency
+# @slug [String] a slug for each currency formatted to work with the CoinMarketCap API (api.coinmarketcap.com)
+# @is_active [Boolean] active status of an existing cryptocurrency
+
+cryptocurrencies = Cryptocurrency.create([
+  { name: 'Bitcoin', max_supply: 21000000, symbol: 'BTC', slug: 'bitcoin', is_active: true }
+  { name: 'Ethereum', symbol: 'ETH', slug: 'ethereum', is_active: true }
+  { name: 'Binance Coin', max_supply: 166801148, symbol: 'BNB', slug: 'binance-coin', is_active: true }
+  { name: 'Tether', symbol: 'USDT', slug: 'tether', is_active: true }
+  { name: 'Solana', symbol: 'SOL', slug: 'solana', is_active: true }
+  { name: "USD Coin", symbol: "USDC", slug: "usd-coin", is_active: true }
+  { name: "Cardano", max_supply: 45000000000, symbol: "ADA", slug: "cardano", is_active: true }
+  { name: "XRP", max_supply: 100000000000, symbol: "XRP", slug: "xrp", is_active: true }
+  { name: "Dogecoin", symbol: "DOGE", slug: "dogecoin", is_active: true }
+  { name: "Polkadot", symbol: "DOT", slug: "polkadot-new", is_active: true }
+])
