21a14aeb6579262d37d17264dbf576f7792f54f5
Patrick Ranchez <ranchezpatrick@gmail.com>
2021-12-15 20:59:22 +0800

    created order form

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,14 @@ class Order < ApplicationRecord
   belongs_to :wallet
   belongs_to :coin
 
-  validates :price, :quantity, presence:true
+  validates :price, :quantity, :kind, presence:true
+  validate :kind_is_buy_or_sell
+
+  def kind_is_buy_or_sell
+    if kind == 'buy' or kind == 'sell'
+      else 
+        errors.add(:kind, "must be buy or sell")
+    end
+  end
+  
 end
