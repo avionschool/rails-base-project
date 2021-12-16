ba19a2e65ba225b373083a46491ab1c7a48aa356
TanTanMa <gelatan12@gmail.com>
2021-12-16 12:27:23 +0800

    added fields to stocks database

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,12 @@
 class Stock < ApplicationRecord
-    belongs_to :user
+    API_KEY= Rails.application.credentials.config[:stocks_api_key]
+
+    def render_stock(ticker)
+        init_api
+        return stock = StockQuote::Stock.quote(ticker)
+    end
+
+    def init_api
+        StockQuote::Stock.new(api_key: API_KEY)
+    end
 end
