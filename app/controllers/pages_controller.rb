4a6832ee25c571b5f5f938b29681be4a50a68654
TanTanMa <gelatan12@gmail.com>
2021-12-16 12:13:02 +0800

    refactored pages controller for sotkcs rendering

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,14 +7,9 @@ class PagesController < ApplicationController
     if params[:ticker] == ""
       @blankSearch = "Please enter stock abbrv"
     elsif params[:ticker]
-      StockQuote::Stock.new(api_key: Rails.application.credentials.config[:stocks_api_key] )
-      @stock = StockQuote::Stock.quote(params[:ticker])
-      # if user entered random codes, UNDER CONSTRUCTION
-      if !@stock
-        @stockErr = "This stock doesnt exist"
+      @stock = Stock.new
+      @stock = @stock.render_stock(params[:ticker])
       end
-
-    end
    
   end
   
