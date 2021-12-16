df711aec4b6f082cd21a6d6ee285cd263d05de20
TanTanMa <gelatan12@gmail.com>
2021-12-16 13:35:54 +0800

    added variables for rendering of stock logos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,16 @@ class PagesController < ApplicationController
 
   def home
     if params[:ticker] == ""
-      @blankSearch = "Please enter stock abbrv"
+      flash[:notice] = "Please enter stock code"
     elsif params[:ticker]
       @stock = Stock.new
+      @logo = Stock.new
       @stock = @stock.render_stock(params[:ticker])
+      @logo = @logo.render_logo(params[:ticker])
+      if !@stock
+        flash[:notice] = "Please enter valid stock code"
       end
-  
+    end
   end
   
   private
