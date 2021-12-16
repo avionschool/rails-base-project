d11150cb7996bccd88fd1f8666e29089cc8481e1
TanTanMa <gelatan12@gmail.com>
2021-12-16 17:30:11 +0800

    added api call for Logo and raw financial data, function for seeding database

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,33 +1,33 @@
 class Stock < ApplicationRecord
     API_KEY= Rails.application.credentials.config[:stocks_api_key]
 
-    def render_stock(ticker)
+    def Stock.render_stock(ticker)
         begin
-        init_api
+        self.init_api
         stock = StockQuote::Stock.quote(ticker)
         rescue
         end
     end
 
-    def render_logo(ticker)
+    def Stock.render_logo(ticker)
         begin
-            init_api
+            self.init_api
             logo = StockQuote::Stock.logo(ticker)
         rescue
         end
     end
 
-    def list_stocks
-        list = ['AAPL', 'GOOG', 'VOO', 'VOOG', 'AMZN', 'MSFT', 'BKSA', 'TSLA', 'DIS']
-        
+    def Stock.list_stocks
+        list = ['AAPL', 'GOOG', 'VOO', 'KO', 'AMZN', 'MSFT', 'FB', 'TSLA', 'DIS']
         list.each do |ticks|
-            render_stock(ticks)
-            create(ticker: ticks, )
+            data = self.render_stock(ticks)
+            logo = self.render_logo(ticks)
+            create(ticker: ticks, logo_url: logo.url, name: data.company_name, current_price: data.latest_price)
         end
         
     end
 
-    def init_api
+    def Stock.init_api
         StockQuote::Stock.new(api_key: API_KEY)
     end
 end
