f5fd792f675cff584ab8ace19a81c9145ebbc685
TanTanMa <gelatan12@gmail.com>
2021-12-16 12:13:51 +0800

    placed API callers in stocks models

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,12 @@
 class Stock < ApplicationRecord
+    API_KEY= Rails.application.credentials.config[:stocks_api_key]
+
+    def render_stock(ticker)
+        init_api
+        return stock = StockQuote::Stock.quote(ticker)
+    end
+
+    def init_api
+        StockQuote::Stock.new(api_key: API_KEY)
+    end
 end
