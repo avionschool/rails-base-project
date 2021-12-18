a26559cf9fbbf5ad02bbe34e37eac6b120172690
TanTanMa <gelatan12@gmail.com>
2021-12-18 12:01:11 +0800

    commented out the search bar function

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,17 +4,13 @@ class PagesController < ApplicationController
   end
 
   def home
-    if params[:ticker] == ""
-      flash[:notice] = "Please enter stock code"
-    elsif params[:ticker]
-      @stock = Stock.new
-      @logo = Stock.new
-      @stock = Stock.render_stock(params[:ticker])
-      @logo = Stock.render_logo(params[:ticker])
-      if !@stock
-        flash[:notice] = "Please enter valid stock code"
-      end
-    end
+    # if params[:ticker] == ""
+    #   flash[:notice] = "Please enter stock code"
+    # elsif params[:ticker]
+    #   @stock = Stock.new
+    #   @stock = Stock.render_stock(params[:ticker])
+    #   byebug
+    # end
   end
   
   private
