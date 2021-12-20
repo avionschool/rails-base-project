51ab136b664c789fae7f2617674e9795566ab1ca
TanTanMa <gelatan12@gmail.com>
2021-12-21 00:36:41 +0800

    migrated the databases changes for users and admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_033322) do
+ActiveRecord::Schema.define(version: 2021_12_20_161427) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "ticker"
+    t.float "prev_price"
+    t.float "amount"
+    t.float "units"
+    t.string "transaction_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
@@ -31,6 +54,9 @@ ActiveRecord::Schema.define(version: 2021_12_16_033322) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "confirmed", default: false, null: false
+    t.string "username"
+    t.index ["confirmed"], name: "index_users_on_confirmed"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
