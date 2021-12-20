fb155b9d48dcc433d24245d5ef2753ed9fffe7d9
TanTanMa <gelatan12@gmail.com>
2021-12-20 12:32:58 +0800

    devise generate admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   resources :stocks
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
