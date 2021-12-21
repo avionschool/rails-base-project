566e4f21048bc151ce772af364a4c51d91503f9c
TanTanMa <gelatan12@gmail.com>
2021-12-21 13:31:30 +0800

    limited the stock routes to only show and index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
 
-  resources :stocks
+  resources :stocks, only: [:index, :show]
   resources :accounts, only: [:new]
 
   #Session routes
