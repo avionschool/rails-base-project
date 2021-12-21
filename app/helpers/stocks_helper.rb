be3ab196fd05b55d3024e62afc88dfa5640729df
TanTanMa <gelatan12@gmail.com>
2021-12-21 13:32:38 +0800

    added currency+interest converter function and render it to stock index page

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,2 +1,7 @@
 module StocksHelper
+    def calc_value(usd_price, buy)
+        usd_to_php = 50
+        converted_rate = usd_price * usd_to_php
+        with_interest_total = buy ? converted_rate + (converted_rate * 0.093) : converted_rate - (converted_rate * 0.093)
+    end
 end
