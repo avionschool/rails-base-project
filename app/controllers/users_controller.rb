1d38ca4022ebe0ab828b9c301a80f9d71bfa0b9e
TanTanMa <gelatan12@gmail.com>
2021-12-21 13:26:32 +0800

    added username in params and text_fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password_digest, :email)
+    params.require(:user).permit(:username, :password_digest, :email, :username)
   end
 
 end
