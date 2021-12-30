b7135934153d87ffd0fe512000b2cdaf831a157e
Kobato13 <johncarlomartinez13@gmail.com>
2021-12-30 11:20:24 +0800

    Added initialized wallet for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_one :wallet
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +7,11 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   # validates :encrypted_password, presence: true
+
+  after_create :intialized_user_wallet
+
+  def intialized_user_wallet
+    wallet_amount = Wallet.new(user_id: id, balance: 100_000)
+    wallet_amount.save
+  end
 end
