9b22b00a2baf6e3bfc698e6d52373b0fb8431e0c
Kobato13 <johncarlomartinez13@gmail.com>
2022-01-04 07:52:16 +0800

    Added total revenue of the current stocks in the stock holdings the user have

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+  def user_stock_revenue(stock)
+    stock_price = stock_value_sell(Stock.find_by(stock_symbol: stock.stock_symbol).current_price)
+    revenue = stock.units * stock_price
+    revenue.round(4)
+  end
+
+  def total_revenue(holdings)
+    revenue = 0
+    holdings.each do |holding|
+      stock_price = stock_value_sell(Stock.find_by(stock_symbol: holding.stock_symbol).current_price)
+      revenue += holding.units * stock_price
+    end
+    revenue.round(4)
+  end
 end
