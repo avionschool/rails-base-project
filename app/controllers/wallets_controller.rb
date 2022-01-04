fd842a68334118612ed869ffc9290ee887e66abe
JM Arenas <jhorene.arenas@gmail.com>
2022-01-04 19:57:57 +0800

    Display all cryptocoins from API

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,5 +1,13 @@
 class WalletsController < ApplicationController
   def index
-    @cryptos = Cryptocurrency.all
+    @api_key = ENV["COINMARKET_API_KEY"]
+    @url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+    @headers = {
+        "X-CMC_PRO_API_KEY" => @api_key,
+        "Accepts" => "application/json"
+    }
+    @request = HTTParty.get(@url, :headers => @headers)
+    
+    @response = JSON.parse(@request.body)["data"]
   end
 end
