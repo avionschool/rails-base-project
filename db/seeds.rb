c095ead69bcec53060615988d896ba9ef3a69790
Kobato13 <johncarlomartinez13@gmail.com>
2022-01-04 10:01:21 +0800

    update db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,12 @@
 
 Admin.create!(email: "admin@test.com", password: "123123")
 
+User.create!(email: "test@user.com", first_name: "Test", last_name: "User", password: "123123", password_confirmation: "123123")
+User.create!(email: "anotheruser@test.com", first_name: "Another User", last_name: "Test", password: "123123", password_confirmation: "123123")
 User.create!(email: "ayaya@ayaya.com", first_name: "AYAYA", last_name: "Test", password: "123123", password_confirmation: "123123")
+
+Stock.create!(stock_symbol: "AA", current_price: "7778")
+Stock.create!(stock_symbol: "BB", current_price: "1112")
+Stock.create!(stock_symbol: "CC", current_price: "20")
+Stock.create!(stock_symbol: "DD", current_price: "30000")
+Stock.create!(stock_symbol: "EE", current_price: "100")
