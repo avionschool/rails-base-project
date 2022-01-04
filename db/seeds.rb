3d3bef433b86a227910dc013c85bbf73dc0e7a0f
Kobato13 <johncarlomartinez13@gmail.com>
2022-01-04 09:53:30 +0800

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(email: "admin@test.com", password: "123123")
+
+User.create!(email: "ayaya@ayaya.com", first_name: "AYAYA", last_name: "Test", password: "123123", password_confirmation: "123123")
