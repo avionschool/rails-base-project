4d6522ddad14908dc57adf481c66f1c8ff320990
JM Arenas <jhorene.arenas@gmail.com>
2022-01-05 18:59:14 +0800

    Removed crypto API request and changed to just a simple wallets controller

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,13 +1,9 @@
 class WalletsController < ApplicationController
   def index
-    @api_key = ENV["COINMARKET_API_KEY"]
-    @url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
-    @headers = {
-        "X-CMC_PRO_API_KEY" => @api_key,
-        "Accepts" => "application/json"
-    }
-    @request = HTTParty.get(@url, :headers => @headers)
-    
-    @response = JSON.parse(@request.body)["data"]
+    @cryptos = Cryptocurrency.all
+  end
+
+  def show
+    @crypto = Cryptocurrency.find(params[:id])
   end
 end
