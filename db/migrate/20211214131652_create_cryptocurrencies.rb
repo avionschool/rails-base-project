b9c597ae63f5b173808ce39e1848dd7a3ad8a244
JM Arenas <jhorene.arenas@gmail.com>
2022-01-05 18:55:30 +0800

    Changed max supply column into currency kind(type)

--- db/migrate/20211214131652_create_cryptocurrencies.rb
+++ db/migrate/20211214131652_create_cryptocurrencies.rb
@@ -2,7 +2,7 @@ class CreateCryptocurrencies < ActiveRecord::Migration[6.0]
   def change
     create_table :cryptocurrencies do |t|
       t.string :name
-      t.bigint :max_supply
+      t.string :kind
       t.string :symbol
       t.string :slug
       t.boolean :is_active
