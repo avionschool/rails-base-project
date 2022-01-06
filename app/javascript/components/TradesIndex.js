fba41aca053bfa0f75c4e46be851c079fa5c7fdb
ccases <carlosmatthewcases@gmail.com>
2022-01-06 20:40:21 +0800

    added headers and fixed margins for frontend

--- app/javascript/components/TradesIndex.js
+++ app/javascript/components/TradesIndex.js
@@ -4,14 +4,23 @@ function TradesIndex({coins}) {
     const [items, setItems] = useState([...coins]);
     const [searchInput, setSearchInput] = useState("");
 
+    const getColorChange = (price) => {
+        return price > 0 
+            ? "green" 
+            : price < 0
+            ? "red"
+            : "plain";
+    }
     const coinItems = items.map((coin) => {
+        const change = getColorChange(coin.usd_24_h);
+        
         return (
             <>
             <a href={coin.base} className="coin-link">
                 <div className = "coin-item">
                     <p className="coin-item-name"> {coin.base}/{coin.target} </p>
-                    <p className="coin-item-price"> {coin.last_price} </p>
-                    <p className="coin-item-change"> {Math.trunc(coin.usd_24_h*100)/100} </p>
+                    <p className={`coin-item-price ${change}`}> {coin.last_price} </p>
+                    <p className={`coin-item-change ${change}`}> {Math.trunc(coin.usd_24_h*100)/100}% </p>
                 </div>
             </a>
             </>
@@ -35,6 +44,12 @@ function TradesIndex({coins}) {
             <div className="trades-index-searchbar">
                 <BsSearch />
                 <input className="trades-index-input" placeholder="Search" type="text" value={searchInput} onChange={e => setSearchInput(e.target.value)} />
+            </div>
+            <div className = "coin-item-header">
+                <p className="coin-item-name"> Pair </p>
+                <p className="coin-item-price"> Price </p>
+                <p className="coin-item-change">
+                 Change </p>
             </div>
             <div className = "trades-index-items">
                 {coinItems}
