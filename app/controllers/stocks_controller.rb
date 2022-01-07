71602c53f4227df79939c30e6880af7da3e66701
TanTanMa <gelatan12@gmail.com>
2022-01-07 17:21:40 +0800

    added stocks, holdings and current user gate for index page

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,6 +3,9 @@ class StocksController < ApplicationController
   # GET /stocks or /stocks.json
   def index
     @stocks = Stock.all
+    @holdings = Holding.all
+    @user_id = params[:user_id]
+    @holdings = Holding.find_by(id: params[:user_id])
   end
 
   def show
@@ -12,5 +15,8 @@ class StocksController < ApplicationController
     # end
   end
   private
+
+  def holding_params 
+  end
 
 end
