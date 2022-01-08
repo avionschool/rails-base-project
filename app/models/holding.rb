eb68de493a1e3c0a813463f1093f20f0d1c20762
TanTanMa <gelatan12@gmail.com>
2022-01-08 12:31:51 +0800

    added validations for holding and stock models, connected trade_log to users

--- app/models/holding.rb
+++ app/models/holding.rb
@@ -1,3 +1,5 @@
 class Holding < ApplicationRecord
     belongs_to :user
+
+    validates :user_id, :ticker, :log_price, presence: true
 end
--- app/models/trade_log.rb
+++ app/models/trade_log.rb
@@ -1,2 +1,3 @@
 class TradeLog < ApplicationRecord
+    belongs_to :user
 end
