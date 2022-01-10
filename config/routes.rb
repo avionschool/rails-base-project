d765d94f89a406899aa9de8d6f7a54455a78f364
jujubean22 <jmevillanueva22@gmail.com>
2022-01-10 20:26:49 +0800

    added index page for admin, buyer, broker and portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :brokers, :controllers => { :registrations => :registrations}
-  devise_for :buyers, :controllers => { :registrations => :registrations}
+  devise_for :admins, :controllers => { :sessions => :sessions }
+  devise_for :brokers, :controllers => { :registrations => :registrations, :sessions => :sessions }
+  devise_for :buyers, :controllers => { :registrations => :registrations, :sessions => :sessions }
   root 'home#index'
+  get 'search_stock' => 'stocks#stock_search'
+  
+  get '/admin/index' => 'admin#index'
+  get '/buyer/index' => 'buyer#index'
+  get '/broker/index' => 'broker#index'
+  get '/broker/portfolio' => 'broker#portfolio'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
