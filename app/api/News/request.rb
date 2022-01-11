a41bd38d7554f7c6944d62af079e02a85ae1bcec
annora <annora@LAPTOP-624LMDFD.localdomain>
2022-01-12 00:11:52 +0800

    added status codes in api news

--- app/api/News/request.rb
+++ app/api/News/request.rb
@@ -12,6 +12,9 @@ module News
                 headers: {'Content-Type'=> 'application/json'}
             )
             JSON.parse(result)
+            {code: result.code, status: 'Success', data:JSON.parse(result)}
+            rescue RestClient::ExceptionWithResponse => error
+            {code: error.http_code, status: error.message, data:Errors.map(error.http_code)}
         end
     end
 end
