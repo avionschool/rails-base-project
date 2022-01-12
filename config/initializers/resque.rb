97d53a23dee7c7dd767dee716d2f5da186f06886
TanTanMa <gelatan12@gmail.com>
2022-01-12 18:50:43 +0800

    removed redis dependency on run

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,1 +1,9 @@
-Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
+require 'resque/server'
+require 'uri'
+
+if Rails.env.development?
+  Resque.redis = Redis.new(host: 'localhost', port: '6379')
+else
+  uri = URI.parse(Rails.application.credentials.redis_heroku[:REDIS_URL])
+  Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+end
