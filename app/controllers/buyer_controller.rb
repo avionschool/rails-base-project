62695c72dbb1a812dbf0b5c916da7ae501d7e251
annora <annora@LAPTOP-624LMDFD.localdomain>
2022-01-12 16:16:03 +0800

    added buyer index welcome div

--- app/controllers/buyer_controller.rb
+++ app/controllers/buyer_controller.rb
@@ -2,7 +2,7 @@ class BuyerController < ApplicationController
   before_action :authenticate_buyer!
 
   def index
-    @globalNews = News::Client.global
-    @exchangeNews = News::Client.exchange
+    # @globalNews = News::Client.global
+    # @exchangeNews = News::Client.exchange
   end
 end
