ccfdcc5d3548420b63de7d99b0b6350dd42b10fd
ccases <carlosmatthewcases@gmail.com>
2022-01-12 19:30:35 +0800

    added initial setup for orders_spec

--- spec/views/orders_spec.rb
+++ spec/views/orders_spec.rb
@@ -1,29 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe "Orders", type: :feature do
+
+  def seed_users
+    User.create([
+      {email: 'cc@avion.com', password: 'kristocurrency', admin: true},
+      {email: 'new1@avion.com', password: 'qwerty', admin: false}
+    ])
+  end
+
+  def seed_coins
+    Coin.create([
+      {
+        base: 'BTC',
+        target: 'USDT',
+        last_price: 42563.2,
+        volume: 3.2,
+        coingecko_id: 'bitcoin',
+        usd_24_h: 9.2
+      },
+      {
+        base: 'ETH',
+        target: 'USDT',
+        last_price: 3265.1,
+        volume: 9.89,
+        coingecko_id: 'ethereum',
+        usd_24_h: 1.26
+      }
+    ])
+  end
+
+  def admin_log_in
+    visit root_path
+    click_link('Sign in')
+    fill_in "user[email]", with: "cc@avion.com"
+    fill_in "user[password]", with: "kristocurrency"
+
+    click_button('Log in')
+  end
+
   describe "on visit" do
+    before {seed_users}
+    before {seed_coins}
+    before {admin_log_in}
     before {visit trade_path("BTC")}
 
-    it "shows sign up and sign in on navbar on visit without credentials" do
-      expect(page).to have_content("Sign up")
-      expect(page).to have_content("Sign in")
+    it "shows coins index" do
+      expect(find('div.trades-index-container')).to be_truthy
     end
 
-    it "signs in right after signing up" do
-      click_link('Sign up')
-      fill_in "user[email]", with: "new69@new.com"
-      fill_in "user[password]", with: "123456"
-      fill_in "user[password_confirmation]", with: "123456"
-
-      select "User", :from => "user[kind]"
-      click_button('Sign up')
-
-      expect(page).to have_content("Wallet")
-      expect(page).to_not have_content("Administrator controls")
-    end
-
-    it "signs out properly" do
-      click_link('Sign Out')
-    end
   end
 end
