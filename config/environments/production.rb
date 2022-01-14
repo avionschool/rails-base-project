b3e179d32eca30341bb77aca0bd259cc49393d33
annora <annora@LAPTOP-624LMDFD.localdomain>
2022-01-15 07:48:45 +0800

    added config to compile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  #compile assets
+  config.serve_static_assets = true
+  config.assets.compile = true
 end
