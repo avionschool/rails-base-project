1368df3cbdd887a4b7124564a144457c808d0107
Josh <josh.uy25@gmail.com>
2022-02-23 23:13:20 +0800

    Fixed authentications for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  has_one :wallet, dependent: :destroy
+  has_many :portfolios, dependent: :destroy
+
+
+  validates :fullname, presence: true, length: { minimum: 4 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
