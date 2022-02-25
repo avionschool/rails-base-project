3c293995fe147782d29b137ac55759476efb0d4a
Argie Barcena <barcenaargie@gmail.com>
2022-02-26 03:26:34 +0800

    modifying environments development and production rb for trying fixing rspec error on git

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
   
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000, protocol: 'http' }
 
 
 
