12865e66db9353dc321e01103ddb0666f6e6b86d
Argie Barcena <barcenaargie@gmail.com>
2022-02-25 21:50:31 +0800

    Fixing rubocop errors indentions

--- app/controllers/traders_controller.rb
+++ app/controllers/traders_controller.rb
@@ -1,6 +1,6 @@
 class TradersController < ApplicationController
   before_action :authenticate_trader!
-  
+
   def index; end
   def show; end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
-    factory :trader do
-        is_approved { true }
-        email { 'test@email.com' }
-        password { 'test12345' }
-        username { 'test_123' }
-        fullname { 'Testing ony' }
-    end
+  factory :trader do
+    is_approved { true }
+    email { 'test@email.com' }
+    password { 'test12345' }
+    username { 'test_123' }
+    fullname { 'Testing ony' }
+  end
 end
