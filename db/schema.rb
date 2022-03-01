b590e87378e76b82687ecf3f2b685ab49c6a2bfb
Josh <josh.uy25@gmail.com>
2022-03-01 18:47:08 +0800

    modified markets db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_24_150943) do
+ActiveRecord::Schema.define(version: 2022_03_01_103847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2022_02_24_150943) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "company_name"
   end
 
   create_table "portfolios", force: :cascade do |t|
