8d128d804e58a8b891c8e04091718a74c6bde0c5
Keith Pineda <keith.m.pineda@gmail.com>
2022-03-01 23:10:46 +0800

    modified user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
   def welcome_email(email)
-    mail(to: email, subject: 'Welcome to iStack')
+    mail(to: email, subject: 'Welcome to iStack') if email != nil
   end
 
   def registration(email)
-    mail(to: email, subject: 'Pending Registration')
+    mail(to: email, subject: 'Pending Registration') if email != nil
   end
 end
