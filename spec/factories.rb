a4d5838c54d7699a93cfa7ada3fb4f0faf61866b
Josh <josh.uy25@gmail.com>
2022-03-03 00:13:57 +0800

    added user request spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,4 +3,18 @@ FactoryBot.define do
     email { 'admin@nofomo.com' }
     password { 'nofomo123' }
   end
+
+  factory :user do
+    fullname { 'example stonks' }
+    email { 'user@example.com' }
+    password { 'test123' }
+    approved { true }
+  end
+  
+  factory :pending_user, class: 'User' do
+    fullname { 'pending stonks' }
+    email { 'pending@example.com' }
+    password { 'test123' }
+    approved { false }
+  end
 end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  let(:user) { create(:user) }
+  let(:pending_user) { create(:pending_user) }
+  
+  it 'sign in user' do
+    sign_in user
+    get stock_markets_path
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'signs out user' do
+    sign_out user
+    get root_path
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'cant sign in pending users' do
+    sign_in pending_user
+    get stock_markets_path
+    expect(response).not_to render_template(:index)
+  end
+end
