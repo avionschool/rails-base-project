b7078d87e01a279f6854465a1fa15b9c38a5321f
Argie Barcena <barcenaargie@gmail.com>
2022-03-04 16:55:02 +0800

    Modifying style for home page and registration page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,21 @@ module.exports = {
     extend: {
         
         screens: {
-          '2xl': {'max': '1535px'},
-          // => @media (max-width: 1535px) { ... }
+          'sm': {'min': '640px', 'max': '767px'},
+          // => @media (min-width: 640px and max-width: 767px) { ... }
     
-          'xl': {'max': '1279px'},
-          // => @media (max-width: 1279px) { ... }
+          'md': {'min': '768px', 'max': '1023px'},
+          // => @media (min-width: 768px and max-width: 1023px) { ... }
     
-          'lg': {'max': '899px'},
-          // => @media (max-width: 1023px) { ... }
+          'lg': {'min': '1024px', 'max': '1279px'},
+          // => @media (min-width: 1024px and max-width: 1279px) { ... }
     
-          'md': {'max': '767px'},
-          // => @media (max-width: 767px) { ... }
+          'xl': {'min': '1280px', 'max': '1535px'},
+          // => @media (min-width: 1280px and max-width: 1535px) { ... }
     
-          'sm': {'max': '639px'},
-          // => @media (max-width: 639px) { ... }
+          '2xl': {'min': '1536px'},
+          // => @media (min-width: 1536px) { ... }
+    
         }
 
 
