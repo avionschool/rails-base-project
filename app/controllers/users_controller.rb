f05f7de108e40cdb946b85edb0f5c7c53271d736
Josh <josh.uy25@gmail.com>
2022-03-07 23:11:57 +0800

    adds profit and loss fixed admin create trader post route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,19 @@ class UsersController < ApplicationController
   def index
     @wallet = current_user.wallet
     @portfolios = current_user.portfolios
+    @profit_and_loss = 0
+    current_market_price = 0
+    portfolio_sum = 0
+
+    if @portfolios.length > 0
+      @portfolios.each do |p|
+        market = Market.find_by(name: p.market_symbol)
+        current_market_price = current_market_price + (market.current_price * p.units).round(2)
+        portfolio_sum = portfolio_sum + (p.hist_price * p.units).round(2)
+      end
+    end
+    @profit_and_loss = (current_market_price - portfolio_sum).round(2)
+      
   end
 
   def show; end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+
+  def get_current_value(portfolio)
+    market = Market.find_by(name: portfolio.market_symbol)
+    return market.current_price * portfolio.units
+  end
 end
